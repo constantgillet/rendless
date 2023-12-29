88a986c51c45005b68ecd07cb9ae8b8c2a4706c8
constant <constant@immortal.game>
2023-12-29 17:31:59 +0100

    Add minscale

--- app/components/ScaleStore.tsx
+++ app/components/ScaleStore.tsx
@@ -8,11 +8,19 @@ interface ScaleState {
   decrease: (by: number) => void;
 }
 
+const minScale = 0.13;
+
 export const useScaleStore = create<ScaleState>()(
   devtools((set) => ({
     scale: 1,
     setScale: (newScale) => set({ scale: newScale }),
     increase: (by) => set((state) => ({ scale: state.scale + by })),
-    decrease: (by) => set((state) => ({ scale: state.scale - by })),
+    decrease: (by) =>
+      set((state) => {
+        const newScale = state.scale - by;
+        return {
+          scale: newScale < minScale ? minScale : newScale,
+        };
+      }),
   }))
 );
