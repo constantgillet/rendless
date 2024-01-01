19fe55c8087b01ab7e0ea106cff55c57e67e57b3
constantgillet <constant.gillet@hetic.net>
2024-01-01 20:16:33 +0100

    Add separator

--- app/components/PropertiesPanel.tsx
+++ app/components/PropertiesPanel.tsx
@@ -16,15 +16,23 @@ export const PropertiesPanel = () => {
         w: 244,
         backgroundColor: "var(--color-panel)",
         borderColor: "var(--global-color-border, currentColor)",
-        padding: "var(--space-3)",
+        padding: "var(--space-4)",
         borderLeft: "1px solid var(--gray-a5)",
       })}
     >
-      <PanelGroup title="Position">
-        <Grid columns="2" gap="2" width="auto">
+      <PanelGroup title="Position & size">
+        <Grid columns="2" gap="4" width="auto">
           <Box>
             <TextField.Root>
-              <TextField.Slot>x</TextField.Slot>
+              <TextField.Slot
+                className={css({
+                  color: "var(--color-text-muted)",
+                  display: "flex",
+                  alignItems: "center",
+                })}
+              >
+                <div>x</div>
+              </TextField.Slot>
               <TextField.Input placeholder="Horizontal" />
             </TextField.Root>
           </Box>
@@ -35,8 +43,8 @@ export const PropertiesPanel = () => {
             </TextField.Root>
           </Box>
         </Grid>
-        <Flex gap={"2"}>
-          <Grid columns="2" gap="2" width="auto">
+        <Flex gap="4">
+          <Grid columns="2" gap="4" width="auto">
             <Box>
               <TextField.Root>
                 <TextField.Slot>w</TextField.Slot>
@@ -50,14 +58,13 @@ export const PropertiesPanel = () => {
               </TextField.Root>
             </Box>
           </Grid>
-          <IconButton variant="soft" highContrast>
-            v
-          </IconButton>
         </Flex>
       </PanelGroup>
-      <Separator className={css({ w: "100%" })} />
-      <PanelGroup title="Fill">
-        <Grid columns="2" gap="2" width="auto">
+      <Separator
+        className={css({ w: "100%!important", marginY: "20px", mx: "-16px" })}
+      />
+      <PanelGroup title="Background">
+        <Grid columns="2" gap="4" width="auto">
           <Box>
             <TextField.Root>
               <TextField.Slot>x</TextField.Slot>
@@ -71,26 +78,29 @@ export const PropertiesPanel = () => {
             </TextField.Root>
           </Box>
         </Grid>
-        <Flex gap={"2"}>
-          <Grid columns="2" gap="2" width="auto">
-            <Box>
-              <TextField.Root>
-                <TextField.Slot>w</TextField.Slot>
-                <TextField.Input placeholder="width" />
-              </TextField.Root>
-            </Box>
-            <Box>
-              <TextField.Root>
-                <TextField.Slot>h</TextField.Slot>
-                <TextField.Input placeholder="height" />
-              </TextField.Root>
-            </Box>
-          </Grid>
-          <IconButton variant="soft" highContrast>
-            v
-          </IconButton>
-        </Flex>
       </PanelGroup>
+      <Separator
+        className={css({ w: "100%!important", marginY: "20px", mx: "-16px" })}
+      />
+      <PanelGroup title="Borders">
+        <Grid columns="2" gap="4" width="auto">
+          <Box>
+            <TextField.Root>
+              <TextField.Slot>x</TextField.Slot>
+              <TextField.Input placeholder="Horizontal" />
+            </TextField.Root>
+          </Box>
+          <Box>
+            <TextField.Root>
+              <TextField.Slot>y</TextField.Slot>
+              <TextField.Input placeholder="Vertical" />
+            </TextField.Root>
+          </Box>
+        </Grid>
+      </PanelGroup>
+      <Separator
+        className={css({ w: "100%!important", marginY: "20px", mx: "-16px" })}
+      />
     </aside>
   );
 };
@@ -104,13 +114,15 @@ const PanelGroup = (props: PanelGroupProps) => {
   return (
     <div
       className={css({
-        py: "24px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "var(--space-3)",
       })}
     >
-      <Text size="3" weight="medium">
+      <Text size="2" className={css({})}>
         {props.title}
       </Text>
-      <Grid columns="1" gap="2" width="auto">
+      <Grid columns="1" gap="4" width="auto">
         {props.children}
       </Grid>
     </div>
