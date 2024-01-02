16a0acf40c2eb404d89aad80e870442768070cba
constantgillet <constant.gillet@hetic.net>
2024-01-03 00:30:05 +0100

    WIP add properties panel

--- app/components/PropertiesPanel.tsx
+++ app/components/PropertiesPanel.tsx
@@ -9,6 +9,7 @@ import {
 } from "@radix-ui/themes";
 import { css } from "styled-system/css";
 import { Icon } from "./Icon";
+import { Tree, useEditorStore } from "./EditorStore";
 
 const Separator = () => {
   return (
@@ -18,7 +19,113 @@ const Separator = () => {
   );
 };
 
+const PositionAndSizeProperties = () => {
+  return (
+    <PanelGroup title="Position & size">
+      <Grid columns="2" gap="4" width="auto">
+        <Box>
+          <TextField.Root>
+            <TextField.Slot
+              className={css({
+                color: "var(--color-text-muted)",
+                display: "flex",
+                alignItems: "center",
+              })}
+            >
+              <div>x</div>
+            </TextField.Slot>
+            <TextField.Input placeholder="Horizontal" />
+          </TextField.Root>
+        </Box>
+        <Box>
+          <TextField.Root>
+            <TextField.Slot>y</TextField.Slot>
+            <TextField.Input placeholder="Vertical" />
+          </TextField.Root>
+        </Box>
+      </Grid>
+      <Flex gap="4">
+        <Grid columns="2" gap="4" width="auto">
+          <Box>
+            <TextField.Root>
+              <TextField.Slot>w</TextField.Slot>
+              <TextField.Input placeholder="width" />
+            </TextField.Root>
+          </Box>
+          <Box>
+            <TextField.Root>
+              <TextField.Slot>h</TextField.Slot>
+              <TextField.Input placeholder="height" />
+            </TextField.Root>
+          </Box>
+        </Grid>
+      </Flex>
+    </PanelGroup>
+  );
+};
+
+const RadiusProperties = () => {
+  return (
+    <PanelGroup title="Radius">
+      <Grid columns="2" gap="4" width="auto">
+        <Box>
+          <TextField.Root>
+            <TextField.Slot>
+              <Icon name="corner-top-left" strokeWidth={2} />
+            </TextField.Slot>
+            <TextField.Input placeholder="Top left" />
+          </TextField.Root>
+        </Box>
+        <Box>
+          <TextField.Root>
+            <TextField.Slot>
+              <Icon name="corner-top-right" strokeWidth={2} />
+            </TextField.Slot>
+            <TextField.Input placeholder="Top right" />
+          </TextField.Root>
+        </Box>
+      </Grid>
+      <Flex gap="4">
+        <Grid columns="2" gap="4" width="auto">
+          <Box>
+            <TextField.Root>
+              <TextField.Slot>
+                <Icon name="corner-bottom-left" strokeWidth={2} />
+              </TextField.Slot>
+              <TextField.Input placeholder="Bottom left" />
+            </TextField.Root>
+          </Box>
+          <Box>
+            <TextField.Root>
+              <TextField.Slot>
+                <Icon name="corner-bottom-right" strokeWidth={2} />
+              </TextField.Slot>
+              <TextField.Input placeholder="Bottom right" />
+            </TextField.Root>
+          </Box>
+        </Grid>
+      </Flex>
+    </PanelGroup>
+  );
+};
+
+const propertiesList = {
+  shape: [
+    {
+      key: "positionsAndSize",
+      component: PositionAndSizeProperties,
+    },
+    {
+      key: "radius",
+      component: RadiusProperties,
+    },
+  ],
+};
+
 export const PropertiesPanel = () => {
+  const selected = useEditorStore((state) => state.selected);
+  const tree = useEditorStore((state) => state.tree);
+
   return (
     <aside
       className={css({
