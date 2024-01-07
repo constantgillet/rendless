aaf9200eddc7239b1c7996362ae9580637b743bc
constantgillet <constant.gillet@hetic.net>
2024-01-07 15:53:10 +0100

    and mapping of multiple colors

--- app/components/BackgroundColorProperties.tsx
+++ app/components/BackgroundColorProperties.tsx
@@ -21,18 +21,42 @@ type BackgroundColorPropertiesProps = {
   };
 };
 
+const groupBySameColor = (properties: ValueType[]) => {
+  //Group by same value and  return array of objects with elementIds and value
+  const colors = properties.reduce((acc, property) => {
+    const color = property.value;
+
+    const index = acc?.findIndex((item) => item.value === color) ?? -1;
+
+    if (index === -1) {
+      acc.push({ elementIds: [property.nodeId], value: color });
+    } else {
+      acc[index].elementIds.push(property.nodeId);
+    }
+
+    return acc;
+  }, [] as { elementIds: string[]; value: string }[]);
+
+  return colors;
+};
+
 export const BackgroundColorProperties = (
   props: BackgroundColorPropertiesProps
 ) => {
   const updateElement = useEditorStore((state) => state.updateElement);
 
-  const [colorValue, setColorValue] = useState("");
+  const [colorValues, setColorValues] = useState(
+    groupBySameColor(props.properties.backgroundColor)
+  );
 
-  const applyColor = (color: string) => {
-    setColorValue(color);
-    props.properties.backgroundColor.forEach((element) => {
+  useEffect(() => {
+    setColorValues(groupBySameColor(props.properties.backgroundColor));
+  }, [props.properties.backgroundColor]);
+
+  const applyColor = (color: string, elementIds: string[]) => {
+    elementIds.forEach((elementId) => {
       updateElement({
-        id: element.nodeId,
+        id: elementId,
         backgroundColor: color,
       });
     });
@@ -40,62 +64,68 @@ export const BackgroundColorProperties = (
 
   return (
     <PanelGroup title="Background color">
-      <Popover.Root>
-        <>
-          <PopoverRadix.Anchor>
-            <div
-              className={css({
-                display: "flex",
-                width: "180px",
-                gap: "8px",
-              })}
-            >
-              <TextField.Root>
-                <TextField.Slot>
-                  <Popover.Trigger onClick={(e) => e.stopPropagation()}>
-                    <button
-                      className={css({
-                        width: "24px",
-                        height: "20px",
-                        flexShrink: 0,
-                        _hover: {
-                          cursor: "pointer",
-                        },
-                        borderRadius: "3px",
-                      })}
-                      style={{
-                        backgroundColor: colorValue,
-                      }}
-                    />
-                  </Popover.Trigger>
-                </TextField.Slot>
-                <TextField.Input
-                  value={colorValue}
+      <Flex direction="column" gap={"2"}>
+        {colorValues.map((color, index) => (
+          <Popover.Root key={index}>
+            <>
+              <PopoverRadix.Anchor>
+                <div
                   className={css({
-                    width: "68px!important",
+                    display: "flex",
+                    width: "180px",
+                    gap: "8px",
                   })}
+                >
+                  <TextField.Root>
+                    <TextField.Slot>
+                      <Popover.Trigger onClick={(e) => e.stopPropagation()}>
+                        <button
+                          className={css({
+                            width: "24px",
+                            height: "20px",
+                            flexShrink: 0,
+                            _hover: {
+                              cursor: "pointer",
+                            },
+                            borderRadius: "3px",
+                          })}
+                          style={{
+                            backgroundColor: color.value,
+                          }}
+                        />
+                      </Popover.Trigger>
+                    </TextField.Slot>
+                    <TextField.Input
+                      value={color.value}
+                      className={css({
+                        width: "68px!important",
+                      })}
+                    />
+                  </TextField.Root>
+                  <TextFieldInput
+                    value={"100%"}
+                    className={css({
+                      width: "52px!important",
+                    })}
+                  />
+                </div>
+              </PopoverRadix.Anchor>
+              <Popover.Content side="left">
+                <SelectPicker.SketchPicker
+                  className={css({
+                    background: "var(--colors-background)!important",
+                    shadow: "none!important",
+                  })}
+                  color={color.value}
+                  onChange={(newColor) =>
+                    applyColor(newColor.hex, color.elementIds)
+                  }
                 />
-              </TextField.Root>
-              <TextFieldInput
-                value={"100%"}
-                className={css({
-                  width: "52px!important",
-                })}
-              />
-            </div>
-          </PopoverRadix.Anchor>
-          <Popover.Content side="left">
-            <SelectPicker.SketchPicker
-              className={css({
-                background: "var(--colors-background)!important",
-                shadow: "none!important",
-              })}
-              color={colorValue}
-              onChange={(color) => applyColor(color.hex)}
-            />
-          </Popover.Content>
-        </>
-      </Popover.Root>
+              </Popover.Content>
+            </>
+          </Popover.Root>
+        ))}
+      </Flex>
     </PanelGroup>
   );
 };
