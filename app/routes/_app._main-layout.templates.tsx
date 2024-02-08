41800661e9416610aa69fe6aa3b4c7ff24d6bb2d
constantgillet <constant.gillet@hetic.net>
2024-02-08 23:18:52 +0100

    add template fetching in laout

--- app/routes/_app._main-layout.templates.tsx
+++ app/routes/_app._main-layout.templates.tsx
@@ -1,9 +1,39 @@
 import { Button, Card, Inset } from "@radix-ui/themes";
-import { useFetcher } from "@remix-run/react";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Link, useFetcher, useLoaderData } from "@remix-run/react";
 import { css } from "styled-system/css";
 import { grid, gridItem } from "styled-system/patterns";
+import { prisma } from "~/libs/prisma";
+import { Tree } from "~/stores/EditorStore";
 
-export default function AppHome() {
+export async function loader({ context }: LoaderFunctionArgs) {
+  const userId = context.user?.id;
+
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+  const templates = await prisma.template.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  //Convert template.tree to Tree type
+  const templatesWithTree = templates.map((template) => {
+    const tree = template.tree as Tree;
+    return {
+      ...template,
+      tree,
+    };
+  });
+
+  return json({ templates: templatesWithTree });
+}
+
+export default function TemplatePage() {
+  const { templates } = useLoaderData<typeof loader>();
+
   const createTemplateFetcher = useFetcher();
 
   const createTemplate = async () => {
@@ -48,46 +78,52 @@ export default function AppHome() {
           </Button>
         </div>
         <div className={grid({ columns: 12, gap: 6 })}>
-          <Card className={gridItem({ colSpan: 4 })}>
-            <Inset
-              className={css({
-                backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='12' height='12' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M10-6V6M10 14v12M26 10H14M6 10H-6'  stroke-linecap='square' stroke-width='0.5' stroke='hsla(226, 70%, 55%, 0.27)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`,
-              })}
-            >
-              <div
+          {templates?.map((template) => (
+            <Card key={template.id} className={gridItem({ colSpan: 4 })}>
+              <Inset
                 className={css({
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "12px",
-                  padding: "14px",
+                  backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='12' height='12' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M10-6V6M10 14v12M26 10H14M6 10H-6'  stroke-linecap='square' stroke-width='0.5' stroke='hsla(226, 70%, 55%, 0.27)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`,
                 })}
               >
                 <div
                   className={css({
-                    aspectRatio: "1.91/1",
-                    rounded: "6px",
-                    overflow: "hidden",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "12px",
+                    padding: "14px",
                   })}
                 >
-                  <img
-                    src="https://assets-global.website-files.com/6040ba28127600ad9182e1be/648ac36424d7794b98b5d3ce_artboard.jpg"
-                    width={"full"}
-                    height={"full"}
-                    alt="Og  template"
-                  />
-                </div>
-                <div className={css({ display: "flex", alignItems: "center" })}>
-                  <div className={css({ flex: 1 })}>template #1</div>
-                  <div className={css({ display: "flex", gap: 2 })}>
-                    <Button size="2">Edition</Button>
-                    <Button size="2" variant="outline" color="red">
-                      Delete
-                    </Button>
+                  <div
+                    className={css({
+                      aspectRatio: "1.91/1",
+                      rounded: "6px",
+                      overflow: "hidden",
+                    })}
+                  >
+                    <img
+                      src="https://assets-global.website-files.com/6040ba28127600ad9182e1be/648ac36424d7794b98b5d3ce_artboard.jpg"
+                      width={"full"}
+                      height={"full"}
+                      alt="Og  template"
+                    />
+                  </div>
+                  <div
+                    className={css({ display: "flex", alignItems: "center" })}
+                  >
+                    <div className={css({ flex: 1 })}>{template.name}</div>
+                    <div className={css({ display: "flex", gap: 2 })}>
+                      <Link to={`/editor/${template.id}`}>
+                        <Button size="2">Edition</Button>{" "}
+                      </Link>
+                      <Button size="2" variant="outline" color="red">
+                        Delete
+                      </Button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Inset>
-          </Card>
+              </Inset>
+            </Card>
+          ))}
         </div>
       </div>
     </>
