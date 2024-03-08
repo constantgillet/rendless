3baf090fda5c90a18f0debae5f0402cc2935a106
constant <constant@immortal.game>
2024-03-08 12:15:39 +0100

    Add content of variable indicator

--- app/components/VariablesFoundIndicator.tsx
+++ app/components/VariablesFoundIndicator.tsx
@@ -18,6 +18,18 @@ const getAllVariablesList = (tree: Tree) => {
       });
     }
 
+    if (node.type === "text") {
+      const matches = node.content.match(/{{(.*?)}}/g);
+      if (matches) {
+        matches.forEach((match) => {
+          const variable = match.replace("{{", "").replace("}}", "");
+          if (!variableList.includes(variable)) {
+            variableList.push(variable);
+          }
+        });
+      }
+    }
+
     if (node.children) {
       node.children.forEach((child) => {
         findVariables(child);
