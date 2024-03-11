d9b1c1dc5209ea9c6093c954c25103f9b4b26bfa
constantgillet <constant.gillet@hetic.net>
2024-03-12 00:21:42 +0100

    Add color line component

--- app/components/BackgroundColorProperties.tsx
+++ app/components/BackgroundColorProperties.tsx
@@ -128,72 +128,93 @@ export const BackgroundColorProperties = (
     >
       <Flex direction="column" gap={"2"}>
         {colorValues.map((color, index) => (
-          <Popover.Root key={index}>
-            <>
-              <PopoverRadix.Anchor>
-                <div className={grid({ columns: 12, gap: 2 })}>
-                  <div className={gridItem({ colSpan: 7 })}>
-                    <PropertyTextField
-                      icon={
-                        <Popover.Trigger onClick={(e) => e.stopPropagation()}>
-                          <button
-                            className={css({
-                              width: "24px",
-                              height: "20px",
-                              flexShrink: 0,
-                              _hover: {
-                                cursor: "pointer",
-                              },
-                              borderRadius: "3px",
-                            })}
-                            style={{
-                              backgroundColor: color.value,
-                            }}
-                          />
-                        </Popover.Trigger>
-                      }
-                      hasVariable={color.colorVariable ? true : false}
-                      placeholder="color hex"
-                      value={color.value}
-                      onChange={(e) => {}}
-                      onBlur={(e) => {}}
-                    />
-                  </div>
-                  <div className={gridItem({ colSpan: 5 })}>
-                    <PropertyTextField
-                      hasVariable={false}
-                      placeholder="height"
-                      value={"100%"}
-                    />
-                  </div>
-                </div>
-              </PopoverRadix.Anchor>
-              <Popover.Content side="left">
-                <SelectPicker.SketchPicker
-                  disableAlpha
-                  styles={{
-                    default: {
-                      picker: {
-                        boxShadow: "none",
-                      },
-                    },
-                  }}
-                  className={css({
-                    background: "var(--colors-background)!important",
-                  })}
-                  color={color.value}
-                  onChange={(newColor) => {
-                    applyColor(newColor.hex, color.elementIds);
-                  }}
-                  onChangeComplete={(newColor) => {
-                    applyColor(newColor.hex, color.elementIds, true);
-                  }}
-                />
-              </Popover.Content>
-            </>
-          </Popover.Root>
+          <ColorLine key={index} color={color} applyColor={applyColor} />
         ))}
       </Flex>
     </PanelGroup>
+  );
+};
+
+type ColorLineProps = {
+  color: {
+    elementIds: string[];
+    value: string;
+    opacity: number;
+    colorVariable?: string | undefined;
+    opacityVariable?: string | undefined;
+  };
+  applyColor: (
+    color: string,
+    elementIds: string[],
+    saveToHistory?: boolean
+  ) => void;
+};
+
+const ColorLine = ({ color, applyColor }: ColorLineProps) => {
+  return (
+    <Popover.Root>
+      <>
+        <PopoverRadix.Anchor>
+          <div className={grid({ columns: 12, gap: 2 })}>
+            <div className={gridItem({ colSpan: 7 })}>
+              <PropertyTextField
+                icon={
+                  <Popover.Trigger onClick={(e) => e.stopPropagation()}>
+                    <button
+                      className={css({
+                        width: "24px",
+                        height: "20px",
+                        flexShrink: 0,
+                        _hover: {
+                          cursor: "pointer",
+                        },
+                        borderRadius: "3px",
+                      })}
+                      style={{
+                        backgroundColor: color.value,
+                      }}
+                    />
+                  </Popover.Trigger>
+                }
+                hasVariable={false}
+                placeholder="color hex"
+                value={color.value}
+                onChange={(e) => {}}
+                onBlur={(e) => {}}
+              />
+            </div>
+            <div className={gridItem({ colSpan: 5 })}>
+              <PropertyTextField
+                hasVariable={false}
+                placeholder="height"
+                value={color.opacity * 100 + "%"}
+              />
+            </div>
+          </div>
+        </PopoverRadix.Anchor>
+        <Popover.Content side="left">
+          <SelectPicker.SketchPicker
+            disableAlpha
+            styles={{
+              default: {
+                picker: {
+                  boxShadow: "none",
+                },
+              },
+            }}
+            className={css({
+              background: "var(--colors-background)!important",
+            })}
+            color={color.value}
+            onChange={(newColor) => {
+              applyColor(newColor.hex, color.elementIds);
+            }}
+            onChangeComplete={(newColor) => {
+              applyColor(newColor.hex, color.elementIds, true);
+            }}
+          />
+        </Popover.Content>
+      </>
+    </Popover.Root>
   );
 };
