6b1fadc2c703ab9c248c1e5639643c6697aef2a0
constantgillet <constant.gillet@hetic.net>
2024-03-17 21:31:34 +0100

    Add account nav current page indicator

--- app/routes/_app._main-layout.account._index.tsx
+++ app/routes/_app._main-layout.account._index.tsx
@@ -1,13 +1,88 @@
+import { Card, Select } from "@radix-ui/themes";
+import { withZod } from "@remix-validated-form/with-zod";
+import { ValidatedForm } from "remix-validated-form";
+import { css } from "styled-system/css";
+import { z } from "zod";
+import { FormSubmitButton } from "~/components/Form";
 import * as m from "~/paraglide/messages";
 
 export const handle = {
   pageTitle: m.settings(),
 };
 
+export const validator = withZod(
+  z.object({
+    email: z
+      .string()
+      .min(1, { message: "Email is required" })
+      .email("Must be a valid email"),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+  })
+);
+
 export default function AccountPage() {
   return (
     <div>
-      <h1>Account</h1>
+      <h1
+        className={css({
+          fontSize: "xl",
+          fontWeight: "bold",
+          color: "var(--gray-11)",
+          marginBottom: "4",
+        })}
+      >
+        General settings
+      </h1>
+      <ValidatedForm
+        validator={validator}
+        method="post"
+        className={css({
+          spaceY: "18px",
+          display: "flex",
+          flexDirection: "column",
+        })}
+      >
+        <div
+          className={css({
+            display: "flex",
+            flexDirection: "column",
+            spaceY: "1",
+          })}
+        >
+          <label>Language</label>
+          <p
+            className={css({
+              color: "var(--gray-11)",
+              fontSize: "sm",
+            })}
+          >
+            Set the language you want to use in the application.{" "}
+          </p>
+          <div
+            className={css({
+              w: "fit",
+            })}
+          >
+            <Select.Root defaultValue="en">
+              <Select.Trigger />
+              <Select.Content>
+                <Select.Item value="en">English</Select.Item>
+                <Select.Item value="fr">French</Select.Item>
+              </Select.Content>
+            </Select.Root>
+          </div>
+        </div>
+        <div
+          className={css({
+            display: "flex",
+            justifyContent: "flex-end",
+          })}
+        >
+          <FormSubmitButton>Save changes</FormSubmitButton>
+        </div>
+      </ValidatedForm>
     </div>
   );
 }
--- app/routes/_app._main-layout.account.tsx
+++ app/routes/_app._main-layout.account.tsx
@@ -1,10 +1,80 @@
-import { Outlet } from "@remix-run/react";
+import { Button } from "@radix-ui/themes";
+import { Link, Outlet, useLocation } from "@remix-run/react";
+import { grid, gridItem } from "styled-system/patterns";
+import { css, cx } from "styled-system/css";
+
+const settingsLinks = [
+  {
+    label: "General",
+    href: ".",
+  },
+  {
+    label: "Security",
+    href: "./security",
+  },
+  {
+    label: "Billing",
+    href: "./billing",
+  },
+];
+
+const isCurrentPage = (pathname: string, href: string) => {
+  //Get last part of pathname
+  const currentPathWithoutParent = pathname.split("/account").pop();
+  const hrefName = href.replace(".", "");
+  return currentPathWithoutParent === hrefName;
+};
 
 export default function SettingsLayout() {
+  const { pathname } = useLocation();
+
   return (
-    <div>
-      outlet
-      <Outlet />
+    <div className={grid({ columns: 12, gap: 8 })}>
+      <div
+        className={cx(
+          gridItem({ colSpan: 3 }),
+          css({
+            display: "flex",
+            flexDirection: "column",
+            spaceY: "4",
+            mt: "2",
+          })
+        )}
+      >
+        {settingsLinks.map((link) => {
+          const isCurrent = isCurrentPage(pathname, link.href);
+
+          return (
+            <Link key={link.href} to={link.href} title={link.label}>
+              <Button
+                variant="ghost"
+                size="3"
+                color="gray"
+                className={css({
+                  w: "100%",
+                  textAlign: "left",
+                })}
+                style={{
+                  background: isCurrent ? "var(--gray-a3)" : undefined,
+                  textAlign: "left",
+                }}
+              >
+                <div
+                  className={css({
+                    w: "100%",
+                    textAlign: "left",
+                  })}
+                >
+                  {link.label}
+                </div>
+              </Button>
+            </Link>
+          );
+        })}
+      </div>
+      <div className={gridItem({ colSpan: 9 })}>
+        <Outlet />
+      </div>
     </div>
   );
 }
