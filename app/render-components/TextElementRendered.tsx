cf543689fddccc6a99174b5cf02fcf56ae0672e3
constantgillet <constant.gillet@hetic.net>
2024-03-28 00:37:23 +0100

    Add font element rendered fontFamily

--- app/render-components/TextElementRendered.tsx
+++ app/render-components/TextElementRendered.tsx
@@ -4,17 +4,13 @@ import { addAlphaToHex } from "~/utils/addAlphaToHex";
 type TextElementProps = ElementText;
 
 export const TextElementRendered = (props: TextElementProps) => {
-  console.log("TextElementRendered", props);
-
   return (
     <p
       style={{
         position: "absolute",
         transform: `translate(${props.x}px, ${props.y}px) rotate(${props.rotate}deg)`,
-        // fontFamily: props.fontFamily,
-        // fontWeight: props.fontWeight,
-        fontFamily: "Roboto",
-        fontWeight: 400,
+        fontFamily: props.fontFamily,
+        fontWeight: props.fontWeight,
         fontSize: `${props.fontSize}px`,
         width: `${props.width}px`,
         height: `${props.height}px`,
