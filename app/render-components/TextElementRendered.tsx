3f57954c29cf4dfc8d6ed7d9d25f1276c46e9c6d
constantgillet <constant.gillet@hetic.net>
2024-03-28 23:58:03 +0100

    add justify content

--- app/render-components/TextElementRendered.tsx
+++ app/render-components/TextElementRendered.tsx
@@ -7,13 +7,22 @@ export const TextElementRendered = (props: TextElementProps) => {
   return (
     <p
       style={{
+        display: "flex",
         position: "absolute",
         transform: `translate(${props.x}px, ${props.y}px) rotate(${props.rotate}deg)`,
         fontFamily: props.fontFamily,
         fontWeight: props.fontWeight,
         fontSize: `${props.fontSize}px`,
+        justifyContent:
+          props.textAlign === "center"
+            ? "center"
+            : props.textAlign === "left"
+            ? "flex-start"
+            : "flex-end",
         width: `${props.width}px`,
         height: `${props.height}px`,
+        textWrap: "wrap",
+        whiteSpace: "pre-wrap",
         color: addAlphaToHex(props.color, props.textColorOpacity),
       }}
     >
