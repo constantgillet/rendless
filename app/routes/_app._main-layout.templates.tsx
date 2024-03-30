a75fca24ab376a5032a5405b8c3242663488bb36
constantgillet <constant.gillet@hetic.net>
2024-03-30 20:07:58 +0100

    Add copy variable name

--- app/routes/_app._main-layout.templates.tsx
+++ app/routes/_app._main-layout.templates.tsx
@@ -1,9 +1,11 @@
-import { Button, Card, Inset } from "@radix-ui/themes";
+import { Button, Card, IconButton, Inset } from "@radix-ui/themes";
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { Link, useFetcher, useLoaderData } from "@remix-run/react";
+import toast from "react-hot-toast";
 import { css } from "styled-system/css";
 import { grid, gridItem } from "styled-system/patterns";
 import { DeleteTemplateButton } from "~/components/DeleteTemplateButton";
+import { Icon } from "~/components/Icon";
 import { Spinner } from "~/components/Spinner";
 import { prisma } from "~/libs/prisma";
 import { Tree } from "~/stores/EditorStore";
@@ -82,52 +84,78 @@ export default function TemplatePage() {
           </Button>
         </div>
         <div className={grid({ columns: 12, gap: 6 })}>
-          {templates?.map((template) => (
-            <Card key={template.id} className={gridItem({ colSpan: 4 })}>
-              <Inset
-                className={css({
-                  backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='12' height='12' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M10-6V6M10 14v12M26 10H14M6 10H-6'  stroke-linecap='square' stroke-width='0.5' stroke='hsla(226, 70%, 55%, 0.27)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`,
-                })}
-              >
-                <div
+          {templates?.map((template) => {
+            const onClickCopyName = (name: string) => {
+              navigator.clipboard.writeText(name);
+              toast.success(`Copied ${name} to clipboard`);
+            };
+            return (
+              <Card key={template.id} className={gridItem({ colSpan: 4 })}>
+                <Inset
                   className={css({
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "12px",
-                    padding: "14px",
+                    backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='12' height='12' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M10-6V6M10 14v12M26 10H14M6 10H-6'  stroke-linecap='square' stroke-width='0.5' stroke='hsla(226, 70%, 55%, 0.27)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`,
                   })}
                 >
                   <div
                     className={css({
-                      aspectRatio: "1.91/1",
-                      rounded: "6px",
-                      overflow: "hidden",
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "12px",
+                      padding: "14px",
                     })}
                   >
-                    <Link to={`/editor/${template.id}`}>
-                      <img
-                        src={`/api/render/${template.name}`}
-                        width={"full"}
-                        height={"full"}
-                        alt="Og  template"
-                      />
-                    </Link>
-                  </div>
-                  <div
-                    className={css({ display: "flex", alignItems: "center" })}
-                  >
-                    <div className={css({ flex: 1 })}>{template.name}</div>
-                    <div className={css({ display: "flex", gap: 2 })}>
+                    <div
+                      className={css({
+                        aspectRatio: "1.91/1",
+                        rounded: "6px",
+                        overflow: "hidden",
+                      })}
+                    >
                       <Link to={`/editor/${template.id}`}>
-                        <Button size="2">Edition</Button>
+                        <img
+                          src={`/api/render/${template.name}`}
+                          width={"full"}
+                          height={"full"}
+                          alt="Og  template"
+                        />
                       </Link>
-                      <DeleteTemplateButton templateId={template.id} />
+                    </div>
+                    <div
+                      className={css({ display: "flex", alignItems: "center" })}
+                    >
+                      <div
+                        className={css({
+                          display: "flex",
+                          flex: 1,
+                          gap: "2",
+                          alignItems: "center",
+                        })}
+                      >
+                        {template.name}
+                        <IconButton
+                          size="1"
+                          aria-label="Copy value"
+                          color="gray"
+                          variant="ghost"
+                        >
+                          <Icon
+                            name="copy"
+                            onClick={() => onClickCopyName(template.name)}
+                          />
+                        </IconButton>
+                      </div>
+                      <div className={css({ display: "flex", gap: 2 })}>
+                        <Link to={`/editor/${template.id}`}>
+                          <Button size="2">Edition</Button>
+                        </Link>
+                        <DeleteTemplateButton templateId={template.id} />
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Inset>
-            </Card>
-          ))}
+                </Inset>
+              </Card>
+            );
+          })}
         </div>
       </div>
     </>
