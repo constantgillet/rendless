7b0b21c985099c938995b61e6a7dd6d46c9bae7e
constantgillet <constant.gillet@hetic.net>
2024-05-03 18:55:36 +0200

    Add recharts

--- app/routes/_app._main-layout.app.tsx
+++ app/routes/_app._main-layout.app.tsx
@@ -8,6 +8,17 @@ import {
 import { css } from "styled-system/css";
 import { Icon } from "~/components/Icon";
 import { prisma } from "~/libs/prisma";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 export async function loader({ context }: LoaderFunctionArgs) {
   const userId = context.user?.id;
@@ -32,7 +43,7 @@ export async function loader({ context }: LoaderFunctionArgs) {
 export default function AppHome() {
   return (
     <>
-      <OnboardingCard />
+      <HomeCard />
     </>
   );
 }
@@ -45,7 +56,45 @@ export const meta: MetaFunction = () => {
   return [{ title: "Home - Rendless" }];
 };
 
-const OnboardingCard = () => {
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    amt: 2100,
+  },
+];
+
+const HomeCard = () => {
   return (
     <Card
       className={css({
@@ -56,7 +105,57 @@ const OnboardingCard = () => {
         className={css({
           p: "16px",
         })}
-      ></div>
+      >
+        <h1
+          className={css({
+            fontSize: "22px",
+            fontWeight: "medium",
+            mb: "16px",
+          })}
+        >
+          Last 7 days renders
+        </h1>
+        <div
+          className={css({
+            mx: "auto",
+            width: "fit-content",
+          })}
+        >
+          <BarChart
+            width={700}
+            height={500}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip
+              wrapperClassName={css({
+                bg: "white",
+                color: "black",
+                border: "1px solid",
+                borderColor: "gray",
+                rounded: "8px",
+              })}
+            />
+            <Legend />
+            <Bar
+              dataKey="uv"
+              fill="var(--accent-9)"
+              activeBar={
+                <Rectangle fill="var(--accent-7)" stroke="var(--accent-12)" />
+              }
+            />
+          </BarChart>
+        </div>
+      </div>
+
       <div
         className={css({
           position: "absolute",
