3e96809725e1fa8211c870d042e0b1013970bea0
constantgillet <rider.constant@gmail.com>
2024-06-03 23:47:35 +0200

    add font weight in render

--- app/routes/api.simple-url.$templateId.tsx
+++ app/routes/api.simple-url.$templateId.tsx
@@ -12,194 +12,194 @@ const cacheEnabled = true;
 
 //TODO prevent duplicated templateName
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
-  const { templateId } = params;
+	const { templateId } = params;
 
-  console.log("RENDER TEMPLATE", templateId);
-  if (!templateId) {
-    throw json(
-      {
-        ok: false,
-        error: {
-          message: "Template id is required",
-          code: "TEMPLATE_IS_REQUIRED",
-        },
-      },
-      { status: 405 }
-    );
-  }
+	console.log("RENDER TEMPLATE", templateId);
+	if (!templateId) {
+		throw json(
+			{
+				ok: false,
+				error: {
+					message: "Template id is required",
+					code: "TEMPLATE_IS_REQUIRED",
+				},
+			},
+			{ status: 405 },
+		);
+	}
 
-  //Get all search params
-  const url = new URL(request.url);
-  const searchParams = url.searchParams;
+	//Get all search params
+	const url = new URL(request.url);
+	const searchParams = url.searchParams;
 
-  const isDraft = searchParams.get("draft") === "true";
+	const isDraft = searchParams.get("draft") === "true";
 
-  const variablesValues = [] as { name: string; value: string }[];
+	const variablesValues = [] as { name: string; value: string }[];
 
-  searchParams.forEach((value, key) => {
-    variablesValues.push({ name: key, value });
-  });
+	searchParams.forEach((value, key) => {
+		variablesValues.push({ name: key, value });
+	});
 
-  //create a hash of the params
-  const variablesValuesHashed = CryptoJS.SHA256(
-    JSON.stringify(variablesValues)
-  ).toString();
+	//create a hash of the params
+	const variablesValuesHashed = CryptoJS.SHA256(
+		JSON.stringify(variablesValues),
+	).toString();
 
-  const cacheKey = `render-${variablesValuesHashed}`;
+	const cacheKey = `render-${variablesValuesHashed}`;
 
-  const imageLocation = `${CACHED_FOLDER}${templateId}/${
-    isDraft ? "draft" : "prod"
-  }/${cacheKey}.png`;
+	const imageLocation = `${CACHED_FOLDER}${templateId}/${
+		isDraft ? "draft" : "prod"
+	}/${cacheKey}.png`;
 
-  const resFileExists = await fileExists(imageLocation);
+	// const resFileExists = await fileExists(imageLocation);
 
-  //If the exit 307 the image is cached
-  if (resFileExists.exists) {
-    return new Response(null, {
-      status: 307,
-      headers: {
-        Location: `${BUCKET_URL}/${imageLocation}`,
-      },
-    });
-  }
+	// //If the exit 307 the image is cached
+	// if (resFileExists.exists) {
+	//   return new Response(null, {
+	//     status: 307,
+	//     headers: {
+	//       Location: `${BUCKET_URL}/${imageLocation}`,
+	//     },
+	//   });
+	// }
 
-  //Get query params
-  //   const url = new URL(`https://og-image/render/${templateName}`);
+	//Get query params
+	//   const url = new URL(`https://og-image/render/${templateName}`);
 
-  //   const urlHashed = CryptoJS.SHA256(url.toString()).toString();
-  //   const cacheKey = `og-image-render-${urlHashed}`;
+	//   const urlHashed = CryptoJS.SHA256(url.toString()).toString();
+	//   const cacheKey = `og-image-render-${urlHashed}`;
 
-  //   const imageLocation = `ogimages/generated/${urlHashed}.png`;
-  //   const imageUrl = `${BUCKET_URL}/${imageLocation}`;
+	//   const imageLocation = `ogimages/generated/${urlHashed}.png`;
+	//   const imageUrl = `${BUCKET_URL}/${imageLocation}`;
 
-  //   try {
-  //     const cachedData = await getCacheData(cacheKey);
+	//   try {
+	//     const cachedData = await getCacheData(cacheKey);
 
-  //     if (cachedData && cacheEnabled) {
-  //       return json({
-  //         ok: true,
-  //         data: {
-  //           url: imageUrl,
-  //         },
-  //       });
-  //     }
-  //   } catch (error) {}
+	//     if (cachedData && cacheEnabled) {
+	//       return json({
+	//         ok: true,
+	//         data: {
+	//           url: imageUrl,
+	//         },
+	//       });
+	//     }
+	//   } catch (error) {}
 
-  let tree: Tree | null = null;
+	let tree: Tree | null = null;
 
-  try {
-    const template = await prisma.template.findFirst({
-      where: {
-        id: templateId,
-      },
-    });
+	try {
+		const template = await prisma.template.findFirst({
+			where: {
+				id: templateId,
+			},
+		});
 
-    if (!template) {
-      return json(
-        {
-          ok: false,
-          error: {
-            message: "Template not found",
-            code: "TEMPLATE_NOT_FOUND",
-          },
-        },
-        { status: 404 }
-      );
-    }
+		if (!template) {
+			return json(
+				{
+					ok: false,
+					error: {
+						message: "Template not found",
+						code: "TEMPLATE_NOT_FOUND",
+					},
+				},
+				{ status: 404 },
+			);
+		}
 
-    // If the template is
+		// If the template is
 
-    tree =
-      template.prodTree === null || isDraft
-        ? (template.tree as unknown as Tree)
-        : (template.prodTree as unknown as Tree);
-  } catch (error) {
-    console.error("Error fetching template", error);
-    throw json(
-      {
-        ok: false,
-        error: {
-          message: "Error fetching template",
-          code: "FETCH_TEMPLATE_ERROR",
-        },
-      },
-      { status: 500 }
-    );
-  }
+		tree =
+			template.prodTree === null || isDraft
+				? (template.tree as unknown as Tree)
+				: (template.prodTree as unknown as Tree);
+	} catch (error) {
+		console.error("Error fetching template", error);
+		throw json(
+			{
+				ok: false,
+				error: {
+					message: "Error fetching template",
+					code: "FETCH_TEMPLATE_ERROR",
+				},
+			},
+			{ status: 500 },
+		);
+	}
 
-  if (!tree) {
-    throw json(
-      {
-        ok: false,
-        error: {
-          message: "Template not found",
-          code: "TEMPLATE_NOT_FOUND",
-        },
-      },
-      { status: 404 }
-    );
-  }
+	if (!tree) {
+		throw json(
+			{
+				ok: false,
+				error: {
+					message: "Template not found",
+					code: "TEMPLATE_NOT_FOUND",
+				},
+			},
+			{ status: 404 },
+		);
+	}
 
-  //https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu7GxKOzY.woff2
+	//https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu7GxKOzY.woff2
 
-  let svg: string | null = null;
+	let svg: string | null = null;
 
-  try {
-    svg = await SvgGenerate(tree, variablesValues);
+	try {
+		svg = await SvgGenerate(tree, variablesValues);
 
-    // console.log(svg);
-  } catch (error) {
-    console.error("Error rendering template", error);
-    throw json(
-      {
-        ok: false,
-        error: {
-          message: "Error rendering template",
-          code: "RENDER_TEMPLATE_ERROR",
-        },
-      },
-      { status: 500 }
-    );
-  }
+		// console.log(svg);
+	} catch (error) {
+		console.error("Error rendering template", error);
+		throw json(
+			{
+				ok: false,
+				error: {
+					message: "Error rendering template",
+					code: "RENDER_TEMPLATE_ERROR",
+				},
+			},
+			{ status: 500 },
+		);
+	}
 
-  const resvg = new Resvg(svg);
+	const resvg = new Resvg(svg);
 
-  try {
-    const pngData = resvg.render();
-    const pngBuffer = pngData.asPng();
+	try {
+		const pngData = resvg.render();
+		const pngBuffer = pngData.asPng();
 
-    //Save the image to S3 as a cache
-    uploadToS3(pngBuffer, imageLocation);
+		//Save the image to S3 as a cache
+		uploadToS3(pngBuffer, imageLocation);
 
-    //Add redis log for doing analytics
-    // const monitorKey = `render:simple:${templateId}:${
-    //   isDraft ? "draft" : "prod"
-    // }:${new Date().getTime()}`;
+		//Add redis log for doing analytics
+		// const monitorKey = `render:simple:${templateId}:${
+		//   isDraft ? "draft" : "prod"
+		// }:${new Date().getTime()}`;
 
-    // //Save the log to redis
-    // setCacheData(
-    //   monitorKey,
-    //   JSON.stringify({
-    //     values: variablesValues,
-    //   })
-    // );
+		// //Save the log to redis
+		// setCacheData(
+		//   monitorKey,
+		//   JSON.stringify({
+		//     values: variablesValues,
+		//   })
+		// );
 
-    return new Response(pngBuffer, {
-      headers: {
-        "Content-Type": "image/png",
-      },
-    });
-  } catch (error) {
-    console.error("Error rendering template", error);
-    throw json(
-      {
-        ok: false,
-        error: {
-          message: "Error rendering template",
-          code: "RENDER_TEMPLATE_ERROR",
-        },
-      },
-      { status: 500 }
-    );
-  }
+		return new Response(pngBuffer, {
+			headers: {
+				"Content-Type": "image/png",
+			},
+		});
+	} catch (error) {
+		console.error("Error rendering template", error);
+		throw json(
+			{
+				ok: false,
+				error: {
+					message: "Error rendering template",
+					code: "RENDER_TEMPLATE_ERROR",
+				},
+			},
+			{ status: 500 },
+		);
+	}
 };
--- app/utils/svgGenerate.tsx
+++ app/utils/svgGenerate.tsx
@@ -5,112 +5,120 @@ import { RectElementRendered } from "~/render-components/RectElementRendered";
 import { TextElementRendered } from "~/render-components/TextElementRendered";
 import type { Tree } from "~/stores/EditorStore";
 
-async function fetchFont(font: string): Promise<ArrayBuffer | null> {
-  const API = `https://fonts.googleapis.com/css2?family=${font}`;
+async function fetchFont(
+	font: string,
+	weight: number,
+	fontStyle: "normal" | "italic" = "normal",
+): Promise<ArrayBuffer | null> {
+	const API = `https://fonts.googleapis.com/css2?family=${font}:wght@${weight}`;
 
-  const css = await (
-    await fetch(API, {
-      headers: {
-        // Make sure it returns TTF.
-        "User-Agent":
-          "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
-      },
-    })
-  ).text();
+	const css = await (
+		await fetch(API, {
+			headers: {
+				// Make sure it returns TTF.
+				"User-Agent":
+					"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+			},
+		})
+	).text();
 
-  const resource = css.match(
-    /src: url\((.+)\) format\('(opentype|truetype)'\)/
-  );
+	const resource = css.match(
+		/src: url\((.+)\) format\('(opentype|truetype)'\)/,
+	);
 
-  if (!resource) return null;
+	if (!resource) return null;
 
-  const res = await fetch(resource[1]);
+	const res = await fetch(resource[1]);
 
-  return res.arrayBuffer();
+	return res.arrayBuffer();
 }
 
 const getAllFonts = async (tree: Tree) => {
-  const fontList = tree.children
-    .filter((child) => child.type === "text")
-    .map((child) => ({
-      fontFamily: child.fontFamily,
-      fontWeight: child.fontWeight,
-      fontStyle: child.fontStyle,
-    }));
+	const fontList = tree.children
+		.filter((child) => child.type === "text")
+		.map((child) => ({
+			fontFamily: child.fontFamily,
+			fontWeight: child.fontWeight,
+			fontStyle: child.fontStyle,
+		}));
 
-  const fonts: Font[] = [];
+	const fonts: Font[] = [];
 
-  for (const font of fontList) {
-    const buffer = await fetchFont(font.fontFamily);
-    if (!buffer) {
-      throw new Error("Font not found");
-    }
+	for (const font of fontList) {
+		const buffer = await fetchFont(
+			font.fontFamily,
+			font.fontWeight,
+			font.fontStyle,
+		);
+		if (!buffer) {
+			throw new Error("Font not found");
+		}
 
-    fonts.push({
-      name: font.fontFamily,
-      data: buffer,
-      weight: font.fontWeight,
-      style: font.fontStyle,
-    });
-  }
+		fonts.push({
+			name: font.fontFamily,
+			data: buffer,
+			weight: font.fontWeight,
+			style: font.fontStyle,
+		});
+	}
 
-  return fonts;
+	return fonts;
 };
 
 export type VariablesValues = { name: string; value: string }[];
 
 export const SvgGenerate = async (
-  tree: Tree,
-  variablesValues: VariablesValues
+	tree: Tree,
+	variablesValues: VariablesValues,
 ) => {
-  const fontsLoaded = await getAllFonts(tree);
+	const fontsLoaded = await getAllFonts(tree);
 
-  const svg = await satori(
-    <TreeToJsx tree={tree} variablesValues={variablesValues} />,
-    {
-      width: 1200,
-      height: 630,
-      fonts: fontsLoaded,
-    }
-  );
+	const svg = await satori(
+		<TreeToJsx tree={tree} variablesValues={variablesValues} />,
+		{
+			width: 1200,
+			height: 630,
+			fonts: fontsLoaded,
+		},
+	);
 
-  return svg;
+	return svg;
 };
 
 const renderedComponentsMap = {
-  rect: RectElementRendered,
-  text: TextElementRendered,
-  image: ImageElementRendered,
+	rect: RectElementRendered,
+	text: TextElementRendered,
+	image: ImageElementRendered,
 };
 
 const TreeToJsx = ({
-  tree,
-  variablesValues,
+	tree,
+	variablesValues,
 }: {
-  tree: Tree;
-  variablesValues: VariablesValues;
+	tree: Tree;
+	variablesValues: VariablesValues;
 }) => {
-  return (
-    <div
-      style={{
-        backgroundColor: tree.backgroundColor,
-        width: 1200,
-        height: 630,
-        position: "relative",
-        display: "flex",
-      }}
-    >
-      {tree.children.map((child) => {
-        const Component = renderedComponentsMap[child.type];
+	return (
+		<div
+			style={{
+				backgroundColor: tree.backgroundColor,
+				width: 1200,
+				height: 630,
+				position: "relative",
+				display: "flex",
+			}}
+		>
+			{tree.children.map((child) => {
+				const Component = renderedComponentsMap[child.type];
 
-        return (
-          <Component
-            key={child.id}
-            variablesValues={variablesValues}
-            {...child}
-          />
-        );
-      })}
-    </div>
-  );
+				return (
+					<Component
+						key={child.id}
+						variablesValues={variablesValues}
+						{...child}
+					/>
+				);
+			})}
+		</div>
+	);
 };
