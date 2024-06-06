c4ddbae1fd67ea4406f7b8277a2739321287c91d
constantgillet <cpcdgillet@gmail.com>
2024-06-06 23:46:43 +0200

    add undo and redo in canvas context menu

--- app/components/CanvasContextMenu.tsx
+++ app/components/CanvasContextMenu.tsx
@@ -1,63 +1,73 @@
 import { ContextMenu } from "@radix-ui/themes";
-import { useEffect } from "react";
+import { type ReactNode, useEffect } from "react";
 import { useEditorStore } from "~/stores/EditorStore";
 import { bringElementsToFront } from "~/stores/actions/bringElementsToFront";
 import { sendElementsToBack } from "~/stores/actions/sendElementsToBack";
 
 type CanvasContextMenuProps = {
-  children: React.ReactNode;
+	children: ReactNode;
 };
 
 export const CanvasContextMenu = (props: CanvasContextMenuProps) => {
-  const selectedItems = useEditorStore((state) => state.selected);
-  const deleteElements = useEditorStore((state) => state.deleteElements);
+	const selectedItems = useEditorStore((state) => state.selected);
+	const deleteElements = useEditorStore((state) => state.deleteElements);
+	const undo = useEditorStore((state) => state.undo);
+	const redo = useEditorStore((state) => state.redo);
 
-  useEffect(() => {
-    const handleContextMenu = (event: MouseEvent) => {
-      event.preventDefault();
-    };
+	useEffect(() => {
+		const handleContextMenu = (event: MouseEvent) => {
+			event.preventDefault();
+		};
 
-    window.addEventListener("contextmenu", handleContextMenu);
+		window.addEventListener("contextmenu", handleContextMenu);
 
-    return () => {
-      window.removeEventListener("contextmenu", handleContextMenu);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("contextmenu", handleContextMenu);
+		};
+	}, []);
 
-  const onClickBringToFront = () => {
-    bringElementsToFront();
-  };
+	const onClickBringToFront = () => {
+		bringElementsToFront();
+	};
 
-  const onClickSendToBack = () => {
-    sendElementsToBack();
-  };
+	const onClickSendToBack = () => {
+		sendElementsToBack();
+	};
 
-  return (
-    <ContextMenu.Root>
-      <ContextMenu.Trigger disabled={selectedItems?.length === 0}>
-        {props.children}
-      </ContextMenu.Trigger>
-      <ContextMenu.Content>
-        {/* <ContextMenu.Item shortcut="⌘ C">Copy</ContextMenu.Item>
+	return (
+		<ContextMenu.Root>
+			<ContextMenu.Trigger>{props.children}</ContextMenu.Trigger>
+			<ContextMenu.Content>
+				{/* <ContextMenu.Item shortcut="⌘ C">Copy</ContextMenu.Item>
         <ContextMenu.Item shortcut="⌘ D">Duplicate</ContextMenu.Item> */}
-        <ContextMenu.Separator />
-        <ContextMenu.Item shortcut="]" onClick={onClickBringToFront}>
-          Bring to front
-        </ContextMenu.Item>
-        <ContextMenu.Item shortcut="[" onClick={onClickSendToBack}>
-          Send to back
-        </ContextMenu.Item>
-        <ContextMenu.Separator />
-        <ContextMenu.Item
-          shortcut="suppr"
-          color="red"
-          onClick={() => {
-            deleteElements(selectedItems);
-          }}
-        >
-          Delete
-        </ContextMenu.Item>
-      </ContextMenu.Content>
-    </ContextMenu.Root>
-  );
+				<ContextMenu.Item shortcut="Ctrl + z" onClick={undo}>
+					Undo
+				</ContextMenu.Item>
+				<ContextMenu.Item shortcut="Ctrl + shift + z" onClick={redo}>
+					Redo
+				</ContextMenu.Item>
+				{selectedItems.length >= 1 ? (
+					<>
+						<ContextMenu.Separator />
+						<ContextMenu.Item shortcut="]" onClick={onClickBringToFront}>
+							Bring to front
+						</ContextMenu.Item>
+						<ContextMenu.Item shortcut="[" onClick={onClickSendToBack}>
+							Send to back
+						</ContextMenu.Item>
+						<ContextMenu.Separator />
+						<ContextMenu.Item
+							shortcut="suppr"
+							color="red"
+							onClick={() => {
+								deleteElements(selectedItems);
+							}}
+						>
+							Delete
+						</ContextMenu.Item>
+					</>
+				) : null}
+			</ContextMenu.Content>
+		</ContextMenu.Root>
+	);
 };
