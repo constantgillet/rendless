80b53e03adca700968f674deefe132655c0701f1
constantgillet <cpcdgillet@gmail.com>
2024-06-12 22:28:01 +0200

    fix box shadow

--- app/render-components/RectElementRendered.tsx
+++ app/render-components/RectElementRendered.tsx
@@ -4,28 +4,28 @@ import { addAlphaToHex } from "~/utils/addAlphaToHex";
 type RectElementProps = RectElement;
 
 export const RectElementRendered = (props: RectElementProps) => {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        transform: `translate(${props.x}px, ${props.y}px) rotate(${props.rotate}deg)`,
-        width: props.width,
-        height: props.height,
-        backgroundColor: addAlphaToHex(
-          props.backgroundColor,
-          props.backgroundOpacity
-        ),
-        borderTopLeftRadius: props.borderTopLeftRadius,
-        borderTopRightRadius: props.borderTopRightRadius,
-        borderBottomLeftRadius: props.borderBottomLeftRadius,
-        borderBottomRightRadius: props.borderBottomRightRadius,
-        boxShadow:
-          props.borderColor && props.borderType && props.borderWidth
-            ? `${props.borderColor} 0px 0px 0px ${props.borderWidth}px ${
-                props.borderType === "inside" ? "inset" : ""
-              }`
-            : "none",
-      }}
-    />
-  );
+	return (
+		<div
+			style={{
+				position: "absolute",
+				transform: `translate(${props.x}px, ${props.y}px) rotate(${props.rotate}deg)`,
+				width: props.width,
+				height: props.height,
+				backgroundColor: addAlphaToHex(
+					props.backgroundColor,
+					props.backgroundOpacity,
+				),
+				borderTopLeftRadius: props.borderTopLeftRadius,
+				borderTopRightRadius: props.borderTopRightRadius,
+				borderBottomLeftRadius: props.borderBottomLeftRadius,
+				borderBottomRightRadius: props.borderBottomRightRadius,
+				boxShadow:
+					props.borderColor && props.borderType && props.borderWidth
+						? `0px 0px 0px ${props.borderWidth}px ${
+								props.borderType === "inside" ? "inset" : ""
+							} ${props.borderColor}`
+						: "none",
+			}}
+		/>
+	);
 };
