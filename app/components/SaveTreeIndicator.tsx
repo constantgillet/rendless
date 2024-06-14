14490e1d2b45e8b8233fc2464b31832b479c7b7f
constantgillet <cpcdgillet@gmail.com>
2024-06-14 20:23:03 +0200

    remove draft

--- app/components/SaveTreeIndicator.tsx
+++ app/components/SaveTreeIndicator.tsx
@@ -7,82 +7,77 @@ import { Icon } from "./Icon";
 import { defaultTree } from "~/constants/defaultTree";
 
 type SaveTreeIndicatorProps = {
-  templateId: string;
-  onSave?: () => void;
+	templateId: string;
 };
 
 export const SaveTreeIndicator = (props: SaveTreeIndicatorProps) => {
-  const { templateId, onSave } = props;
+	const { templateId } = props;
 
-  const [isSaving, setIsSaving] = useState(false);
-  const [isSaved, setIsSaved] = useState(true);
+	const [isSaving, setIsSaving] = useState(false);
+	const [isSaved, setIsSaved] = useState(true);
 
-  const tree = useEditorStore((state) => state.tree);
+	const tree = useEditorStore((state) => state.tree);
 
-  const [debouncedTree, setValue] = useDebounceValue(tree, 1500);
+	const [debouncedTree, setValue] = useDebounceValue(tree, 1500);
 
-  const saveTree = () => {
-    //Prevent save of the initial default tree
-    if (tree === defaultTree) return;
+	const saveTree = () => {
+		//Prevent save of the initial default tree
+		if (tree === defaultTree) return;
 
-    setIsSaving(true);
-    const formData = new FormData();
+		setIsSaving(true);
+		const formData = new FormData();
 
-    formData.append("templateId", templateId);
-    formData.append("tree", JSON.stringify(tree));
+		formData.append("templateId", templateId);
+		formData.append("tree", JSON.stringify(tree));
 
-    fetch("/api/update-template", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setIsSaved(true);
+		fetch("/api/update-template", {
+			method: "POST",
+			body: formData,
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				setIsSaved(true);
 
-        if (data.changed) {
-          onSave?.();
-        }
+				setIsSaving(false);
+			})
+			.catch((error) => {
+				console.error("Error saving tree", error);
+				setIsSaved(false);
+				setIsSaving(false);
+			});
+	};
 
-        setIsSaving(false);
-      })
-      .catch((error) => {
-        console.error("Error saving tree", error);
-        setIsSaved(false);
-        setIsSaving(false);
-      });
-  };
+	useEffect(() => {
+		setIsSaved(false);
+		setValue(tree);
+	}, [tree]);
 
-  useEffect(() => {
-    setIsSaved(false);
-    setValue(tree);
-  }, [tree]);
+	useEffect(() => {
+		if (debouncedTree) {
+			saveTree();
+		}
+	}, [debouncedTree]);
 
-  useEffect(() => {
-    if (debouncedTree) {
-      saveTree();
-    }
-  }, [debouncedTree]);
-
-  return (
-    <div
-      className={css({
-        display: "flex",
-        color: "var(--gray-a11)",
-        alignItems: "center",
-        gap: "1",
-        fontSize: "sm",
-      })}
-    >
-      {isSaved ? (
-        <>
-          <Icon name="checkmark-circle" fill="var(--green-10)" /> Saved
-        </>
-      ) : (
-        <>
-          {isSaving ? <Spinner size={12} /> : <Icon name="info" />} Changes
-          unsaved
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div
+			className={css({
+				display: "flex",
+				color: "var(--gray-a11)",
+				alignItems: "center",
+				gap: "1",
+				fontSize: "sm",
+			})}
+		>
+			{isSaved ? (
+				<>
+					<Icon name="checkmark-circle" fill="var(--green-10)" /> Saved
+				</>
+			) : (
+				<>
+					{isSaving ? <Spinner size={12} /> : <Icon name="info" />} Changes
+					unsaved
+				</>
+			)}
+		</div>
+	);
 };
--- app/routes/_app._main-layout.templates.tsx
+++ app/routes/_app._main-layout.templates.tsx
@@ -1,15 +1,15 @@
 import {
-  Badge,
-  Button,
-  Card,
-  DropdownMenu,
-  IconButton,
-  Inset,
+	Badge,
+	Button,
+	Card,
+	DropdownMenu,
+	IconButton,
+	Inset,
 } from "@radix-ui/themes";
 import {
-  type LoaderFunctionArgs,
-  type MetaFunction,
-  json,
+	type LoaderFunctionArgs,
+	type MetaFunction,
+	json,
 } from "@remix-run/node";
 import { Link, useFetcher, useLoaderData } from "@remix-run/react";
 import { useState } from "react";
@@ -25,276 +25,276 @@ import type { Tree } from "~/stores/EditorStore";
 import { getAllVariablesList } from "~/utils/getAllVariablesList";
 
 export async function loader({ context }: LoaderFunctionArgs) {
-  const userId = context.user?.id;
+	const userId = context.user?.id;
 
-  if (!userId) {
-    throw new Error("User not found");
-  }
+	if (!userId) {
+		throw new Error("User not found");
+	}
 
-  const templates = await prisma.template.findMany({
-    where: {
-      userId,
-    },
-  });
+	const templates = await prisma.template.findMany({
+		where: {
+			userId,
+		},
+	});
 
-  //Convert template.tree to Tree type
-  const templatesWithTree = templates.map((template) => {
-    const tree = template.tree as Tree;
-    return {
-      ...template,
-      tree,
-    };
-  });
+	//Convert template.tree to Tree type
+	const templatesWithTree = templates.map((template) => {
+		const tree = template.tree as Tree;
+		return {
+			...template,
+			tree,
+		};
+	});
 
-  return json({ templates: templatesWithTree });
+	return json({ templates: templatesWithTree });
 }
 
 export default function TemplatePage() {
-  const { templates } = useLoaderData<typeof loader>();
+	const { templates } = useLoaderData<typeof loader>();
 
-  const createTemplateFetcher = useFetcher();
+	const createTemplateFetcher = useFetcher();
 
-  const createTemplate = async () => {
-    createTemplateFetcher.submit(
-      {},
-      {
-        action: "/api/create-template",
-        method: "POST",
-      }
-    );
-  };
+	const createTemplate = async () => {
+		createTemplateFetcher.submit(
+			{},
+			{
+				action: "/api/create-template",
+				method: "POST",
+			},
+		);
+	};
 
-  const onClickCreateButton = () => {
-    if (
-      createTemplateFetcher.state === "loading" ||
-      createTemplateFetcher.state === "submitting"
-    )
-      return;
+	const onClickCreateButton = () => {
+		if (
+			createTemplateFetcher.state === "loading" ||
+			createTemplateFetcher.state === "submitting"
+		)
+			return;
 
-    createTemplate();
-  };
+		createTemplate();
+	};
 
-  return (
-    <>
-      <div className={css({ spaceY: "4" })}>
-        <div
-          className={css({
-            display: "flex",
-            justifyContent: "space-between",
-          })}
-        >
-          <div
-            className={css({
-              fontSize: "medium",
-              color: "var(--gray-a11)",
-            })}
-          >
-            You will discover here all your images templates
-          </div>
-          <Button variant="classic" onClick={onClickCreateButton}>
-            {createTemplateFetcher.state === "loading" ||
-              (createTemplateFetcher.state === "submitting" && <Spinner />)}
-            Create a template
-          </Button>
-        </div>
-        {templates?.length > 0 ? (
-          <div className={grid({ columns: 12, gap: 6 })}>
-            {templates?.map((template) => {
-              return <TemplateCard key={template.id} template={template} />;
-            })}
-          </div>
-        ) : (
-          <div>
-            <div
-              className={css({
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                gap: "4",
-                color: "var(--gray-a11)",
-                flexDirection: "column",
-                py: "64px",
-              })}
-            >
-              <Icon name="image" size="4xl" />
-              <div className={css({ fontSize: "16px" })}>
-                There is no template yet
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={css({ spaceY: "4" })}>
+				<div
+					className={css({
+						display: "flex",
+						justifyContent: "space-between",
+					})}
+				>
+					<div
+						className={css({
+							fontSize: "medium",
+							color: "var(--gray-a11)",
+						})}
+					>
+						You will discover here all your images templates
+					</div>
+					<Button variant="classic" onClick={onClickCreateButton}>
+						{createTemplateFetcher.state === "loading" ||
+							(createTemplateFetcher.state === "submitting" && <Spinner />)}
+						Create a template
+					</Button>
+				</div>
+				{templates?.length > 0 ? (
+					<div className={grid({ columns: 12, gap: 6 })}>
+						{templates?.map((template) => {
+							return <TemplateCard key={template.id} template={template} />;
+						})}
+					</div>
+				) : (
+					<div>
+						<div
+							className={css({
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+								gap: "4",
+								color: "var(--gray-a11)",
+								flexDirection: "column",
+								py: "64px",
+							})}
+						>
+							<Icon name="image" size="4xl" />
+							<div className={css({ fontSize: "16px" })}>
+								There is no template yet
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
 const onClickCopyId = (id: string) => {
-  navigator.clipboard.writeText(id);
-  toast.success(`Copied ${id} to clipboard`);
+	navigator.clipboard.writeText(id);
+	toast.success(`Copied ${id} to clipboard`);
 };
 
 const TemplateCard = ({
-  template,
+	template,
 }: {
-  template: {
-    id: string;
-    name: string;
-    tree: Tree;
-  };
+	template: {
+		id: string;
+		name: string;
+		tree: Tree;
+	};
 }) => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [useTemplateDialogOpen, setUseTemplateDialogOpen] = useState(false);
+	const [modalOpen, setModalOpen] = useState(false);
+	const [useTemplateDialogOpen, setUseTemplateDialogOpen] = useState(false);
 
-  const variables = getAllVariablesList(template.tree);
+	const variables = getAllVariablesList(template.tree);
 
-  return (
-    <Card key={template.id} className={gridItem({ colSpan: 4 })}>
-      <Inset
-        className={css({
-          backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='12' height='12' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M10-6V6M10 14v12M26 10H14M6 10H-6'  stroke-linecap='square' stroke-width='0.5' stroke='hsla(226, 70%, 55%, 0.27)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`,
-        })}
-      >
-        <div
-          className={css({
-            display: "flex",
-            flexDirection: "column",
-            gap: "12px",
-            padding: "14px",
-          })}
-        >
-          <div
-            className={css({
-              aspectRatio: "1.91/1",
-              rounded: "6px",
-              overflow: "hidden",
-            })}
-          >
-            <Link to={`/editor/${template.id}`}>
-              <img
-                src={`/api/simple-url/${template.id}?draft=true`}
-                width={"full"}
-                height={"full"}
-                alt="Og  template"
-              />
-            </Link>
-          </div>
-          <div
-            className={css({
-              display: "flex",
-              alignItems: "center",
-            })}
-          >
-            <div
-              className={css({
-                display: "flex",
-                flex: 1,
-                gap: "2",
-                alignItems: "center",
-              })}
-            >
-              {template.name}
-              <Badge>{template.id}</Badge>
-              <IconButton
-                size="1"
-                aria-label="Copy value"
-                color="gray"
-                variant="ghost"
-              >
-                <Icon name="copy" onClick={() => onClickCopyId(template.id)} />
-              </IconButton>{" "}
-            </div>
-            <div></div>
-            <div className={css({ display: "flex", gap: 2 })}>
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Button variant="surface" highContrast>
-                    Actions <Icon name="chevron-down" />
-                  </Button>
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <Link to={`/editor/${template.id}`}>
-                    <DropdownMenu.Item>Edit</DropdownMenu.Item>
-                  </Link>
-                  <DropdownMenu.Item
-                    onClick={() => setUseTemplateDialogOpen(true)}
-                  >
-                    Use template
-                  </DropdownMenu.Item>
-                  <DuplicateTemplateDropdownItem templateId={template.id} />
+	return (
+		<Card key={template.id} className={gridItem({ colSpan: 4 })}>
+			<Inset
+				className={css({
+					backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='12' height='12' patternTransform='scale(1) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 100%, 0)'/><path d='M10-6V6M10 14v12M26 10H14M6 10H-6'  stroke-linecap='square' stroke-width='0.5' stroke='hsla(226, 70%, 55%, 0.27)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`,
+				})}
+			>
+				<div
+					className={css({
+						display: "flex",
+						flexDirection: "column",
+						gap: "12px",
+						padding: "14px",
+					})}
+				>
+					<div
+						className={css({
+							aspectRatio: "1.91/1",
+							rounded: "6px",
+							overflow: "hidden",
+						})}
+					>
+						<Link to={`/editor/${template.id}`}>
+							<img
+								src={`/api/simple-url/${template.id}`}
+								width={"full"}
+								height={"full"}
+								alt="Og  template"
+							/>
+						</Link>
+					</div>
+					<div
+						className={css({
+							display: "flex",
+							alignItems: "center",
+						})}
+					>
+						<div
+							className={css({
+								display: "flex",
+								flex: 1,
+								gap: "2",
+								alignItems: "center",
+							})}
+						>
+							{template.name}
+							<Badge>{template.id}</Badge>
+							<IconButton
+								size="1"
+								aria-label="Copy value"
+								color="gray"
+								variant="ghost"
+							>
+								<Icon name="copy" onClick={() => onClickCopyId(template.id)} />
+							</IconButton>{" "}
+						</div>
+						<div></div>
+						<div className={css({ display: "flex", gap: 2 })}>
+							<DropdownMenu.Root>
+								<DropdownMenu.Trigger>
+									<Button variant="surface" highContrast>
+										Actions <Icon name="chevron-down" />
+									</Button>
+								</DropdownMenu.Trigger>
+								<DropdownMenu.Content>
+									<Link to={`/editor/${template.id}`}>
+										<DropdownMenu.Item>Edit</DropdownMenu.Item>
+									</Link>
+									<DropdownMenu.Item
+										onClick={() => setUseTemplateDialogOpen(true)}
+									>
+										Use template
+									</DropdownMenu.Item>
+									<DuplicateTemplateDropdownItem templateId={template.id} />
 
-                  <DropdownMenu.Separator />
-                  <DropdownMenu.Item
-                    color="red"
-                    onClick={() => setModalOpen(true)}
-                  >
-                    Delete
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-              <UseTemplateDialog
-                templateId={template.id}
-                open={useTemplateDialogOpen}
-                onOpenChange={(open) => setUseTemplateDialogOpen(open)}
-                variables={variables}
-              />
-              <DeleteTemplateModal
-                templateId={template.id}
-                open={modalOpen}
-                onOpenChange={(open) => setModalOpen(open)}
-              />
-              {/* <Link to={`/editor/${template.id}`}>
+									<DropdownMenu.Separator />
+									<DropdownMenu.Item
+										color="red"
+										onClick={() => setModalOpen(true)}
+									>
+										Delete
+									</DropdownMenu.Item>
+								</DropdownMenu.Content>
+							</DropdownMenu.Root>
+							<UseTemplateDialog
+								templateId={template.id}
+								open={useTemplateDialogOpen}
+								onOpenChange={(open) => setUseTemplateDialogOpen(open)}
+								variables={variables}
+							/>
+							<DeleteTemplateModal
+								templateId={template.id}
+								open={modalOpen}
+								onOpenChange={(open) => setModalOpen(open)}
+							/>
+							{/* <Link to={`/editor/${template.id}`}>
             <Button size="2">Edition</Button>
           </Link>
           <DeleteTemplateButton templateId={template.id} /> */}
-            </div>
-          </div>
-        </div>
-      </Inset>
-    </Card>
-  );
+						</div>
+					</div>
+				</div>
+			</Inset>
+		</Card>
+	);
 };
 
 const DuplicateTemplateDropdownItem = ({
-  templateId,
+	templateId,
 }: {
-  templateId: string;
+	templateId: string;
 }) => {
-  const duplicateTemplateFetcher = useFetcher();
+	const duplicateTemplateFetcher = useFetcher();
 
-  const duplicateTemplate = async () => {
-    duplicateTemplateFetcher.submit(
-      {
-        fromTemplateId: templateId,
-      },
-      {
-        action: "/api/create-template",
-        method: "POST",
-      }
-    );
-  };
+	const duplicateTemplate = async () => {
+		duplicateTemplateFetcher.submit(
+			{
+				fromTemplateId: templateId,
+			},
+			{
+				action: "/api/create-template",
+				method: "POST",
+			},
+		);
+	};
 
-  const onClickDuplicateButton = () => {
-    if (
-      duplicateTemplateFetcher.state === "loading" ||
-      duplicateTemplateFetcher.state === "submitting"
-    )
-      return;
+	const onClickDuplicateButton = () => {
+		if (
+			duplicateTemplateFetcher.state === "loading" ||
+			duplicateTemplateFetcher.state === "submitting"
+		)
+			return;
 
-    duplicateTemplate();
-  };
+		duplicateTemplate();
+	};
 
-  return (
-    <DropdownMenu.Item onClick={onClickDuplicateButton}>
-      {duplicateTemplateFetcher.state === "loading" ||
-        (duplicateTemplateFetcher.state === "submitting" && <Spinner />)}
-      Duplicate
-    </DropdownMenu.Item>
-  );
+	return (
+		<DropdownMenu.Item onClick={onClickDuplicateButton}>
+			{duplicateTemplateFetcher.state === "loading" ||
+				(duplicateTemplateFetcher.state === "submitting" && <Spinner />)}
+			Duplicate
+		</DropdownMenu.Item>
+	);
 };
 
 export const handle = {
-  pageTitle: "templates",
+	pageTitle: "templates",
 };
 
 export const meta: MetaFunction = () => {
-  return [{ title: "Templates - Rendless" }];
+	return [{ title: "Templates - Rendless" }];
 };
