63292f45ad33924a348202cabdfbdee5ae120e31
constantgillet <cpcdgillet@gmail.com>
2024-06-15 20:32:27 +0200

    add text shadow in elements

--- app/components/TextElement.tsx
+++ app/components/TextElement.tsx
@@ -10,138 +10,145 @@ import type { TextElement as TextElementType } from "~/stores/elementTypes";
 type TextElementProps = TextElementType;
 
 export const TextElement = (props: TextElementProps) => {
-  const scale = useScaleStore((state) => state.scale);
-  const textElementRef = useRef<HTMLTextAreaElement>(null);
-  const [isEditingContent, setIsEditingContent] = useState(false);
+	const scale = useScaleStore((state) => state.scale);
+	const textElementRef = useRef<HTMLTextAreaElement>(null);
+	const [isEditingContent, setIsEditingContent] = useState(false);
 
-  const updateElement = useEditorStore((state) => state.updateElement);
-  const updateElements = useEditorStore((state) => state.updateElements);
+	const updateElement = useEditorStore((state) => state.updateElement);
+	const updateElements = useEditorStore((state) => state.updateElements);
 
-  const onChangeDebounce = useDebounce((content: string) => {
-    console.log("onChangeDebounce");
+	const onChangeDebounce = useDebounce((content: string) => {
+		console.log("onChangeDebounce");
 
-    updateElements(
-      [
-        {
-          id: props.id,
-          content: content,
-        },
-      ],
-      true
-    );
-  }, 1000);
+		updateElements(
+			[
+				{
+					id: props.id,
+					content: content,
+				},
+			],
+			true,
+		);
+	}, 1000);
 
-  const handleClick = (event: React.MouseEventHandler<HTMLTextAreaElement>) => {
-    if (event.detail === 2) {
-      textElementRef.current?.focus();
-      setIsEditingContent(true);
-    }
-  };
+	const handleClick = (event: React.MouseEventHandler<HTMLTextAreaElement>) => {
+		if (event.detail === 2) {
+			textElementRef.current?.focus();
+			setIsEditingContent(true);
+		}
+	};
 
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
-        setIsEditingContent(false);
-      }
-    };
+	useEffect(() => {
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === "Escape") {
+				setIsEditingContent(false);
+			}
+		};
 
-    window.addEventListener("keydown", handleKeyDown);
+		window.addEventListener("keydown", handleKeyDown);
 
-    //If clicked outside textarea, stop editing
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        textElementRef.current &&
-        !textElementRef.current.contains(event.target as Node)
-      ) {
-        setIsEditingContent(false);
-      }
-    };
+		//If clicked outside textarea, stop editing
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				textElementRef.current &&
+				!textElementRef.current.contains(event.target as Node)
+			) {
+				setIsEditingContent(false);
+			}
+		};
 
-    window.addEventListener("mousedown", handleClickOutside);
+		window.addEventListener("mousedown", handleClickOutside);
 
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+			window.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, []);
 
-  const resizeTextArea = () => {
-    if (!textElementRef.current) return;
+	const resizeTextArea = () => {
+		if (!textElementRef.current) return;
 
-    textElementRef.current.style.height = "auto";
-    textElementRef.current.style.height =
-      textElementRef.current.scrollHeight + "px";
-  };
+		textElementRef.current.style.height = "auto";
+		textElementRef.current.style.height =
+			textElementRef.current.scrollHeight + "px";
+	};
 
-  useEffect(resizeTextArea, [
-    props.content,
-    props.fontSize,
-    props.width,
-    scale,
-  ]);
+	useEffect(resizeTextArea, [
+		props.content,
+		props.fontSize,
+		props.width,
+		scale,
+	]);
 
-  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    updateElement({
-      id: props.id,
-      content: event.target.value,
-    });
+	const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		updateElement({
+			id: props.id,
+			content: event.target.value,
+		});
 
-    onChangeDebounce(event.target.value);
-  };
+		onChangeDebounce(event.target.value);
+	};
 
-  return (
-    <div
-      className={cx(
-        "target",
-        css({
-          position: "absolute",
-          _hover: {
-            outline: "1px solid #4af",
-            outlineOffset: "-1px",
-          },
-          _focusVisible: {
-            outline: "none",
-          },
-        })
-      )}
-      style={{
-        transform: `translate(${props.x}px, ${props.y}px) rotate(${props.rotate}deg)`,
-        width: props.width,
-        height: props.height,
-      }}
-      {...{
-        [DATA_SCENA_ELEMENT_ID]: props.id,
-      }}
-    >
-      <textarea
-        ref={textElementRef}
-        value={props.content}
-        onChange={(e) => onChange(e)}
-        className={cx(
-          css({
-            width: "100%",
-            minHeight: "100%",
-            resize: "none",
-            _focusVisible: {
-              outline: "none",
-            },
-          })
-        )}
-        style={{
-          fontFamily: props.fontFamily,
-          fontSize: `${props.fontSize}px`,
-          fontWeight: props.fontWeight,
-          fontStyle: props.fontStyle,
-          background: "transparent",
-          color: addAlphaToHex(props.color, props.textColorOpacity),
-          cursor: isEditingContent ? "text" : "default",
-          textAlign: props.textAlign,
-          textTransform: props.textTransform,
-          lineHeight: props.lineHeight,
-          filter: props.blur ? `blur(${props.blur}px)` : "none",
-        }}
-        onClick={handleClick}
-      />
-    </div>
-  );
+	return (
+		<div
+			className={cx(
+				"target",
+				css({
+					position: "absolute",
+					_hover: {
+						outline: "1px solid #4af",
+						outlineOffset: "-1px",
+					},
+					_focusVisible: {
+						outline: "none",
+					},
+				}),
+			)}
+			style={{
+				transform: `translate(${props.x}px, ${props.y}px) rotate(${props.rotate}deg)`,
+				width: props.width,
+				height: props.height,
+			}}
+			{...{
+				[DATA_SCENA_ELEMENT_ID]: props.id,
+			}}
+		>
+			<textarea
+				ref={textElementRef}
+				value={props.content}
+				onChange={(e) => onChange(e)}
+				className={cx(
+					css({
+						width: "100%",
+						minHeight: "100%",
+						resize: "none",
+						_focusVisible: {
+							outline: "none",
+						},
+					}),
+				)}
+				style={{
+					fontFamily: props.fontFamily,
+					fontSize: `${props.fontSize}px`,
+					fontWeight: props.fontWeight,
+					fontStyle: props.fontStyle,
+					background: "transparent",
+					color: addAlphaToHex(props.color, props.textColorOpacity),
+					cursor: isEditingContent ? "text" : "default",
+					textAlign: props.textAlign,
+					textTransform: props.textTransform,
+					lineHeight: props.lineHeight,
+					filter: props.blur ? `blur(${props.blur}px)` : "none",
+					textShadow:
+						props.textShadowXOffset &&
+						props.textShadowYOffset &&
+						props.textShadowBlur &&
+						props.textShadowColor
+							? `${props.textShadowXOffset}px ${props.textShadowYOffset}px ${props.textShadowBlur}px ${props.textShadowColor}`
+							: "none",
+				}}
+				onClick={handleClick}
+			/>
+		</div>
+	);
 };
--- app/render-components/TextElementRendered.tsx
+++ app/render-components/TextElementRendered.tsx
@@ -3,58 +3,65 @@ import { addAlphaToHex } from "~/utils/addAlphaToHex";
 import type { TextElement } from "~/stores/elementTypes";
 
 type TextElementProps = TextElement & {
-  variablesValues: VariablesValues;
+	variablesValues: VariablesValues;
 };
 
 const setValueFromVariable = (
-  value: string,
-  variablesValues: VariablesValues
+	value: string,
+	variablesValues: VariablesValues,
 ) => {
-  const variable = variablesValues.find((variable) => variable.name === value);
+	const variable = variablesValues.find((variable) => variable.name === value);
 
-  return variable ? variable.value : value;
+	return variable ? variable.value : value;
 };
 
 export const TextElementRendered = (props: TextElementProps) => {
-  const contentWithVariables = props.content.replace(
-    /{{(.*?)}}/g,
-    (_, match) => {
-      const variable = props.variablesValues.find(
-        (variable) => variable.name === match
-      );
+	const contentWithVariables = props.content.replace(
+		/{{(.*?)}}/g,
+		(_, match) => {
+			const variable = props.variablesValues.find(
+				(variable) => variable.name === match,
+			);
 
-      return variable ? variable.value : `{{${match}}}`;
-    }
-  );
+			return variable ? variable.value : `{{${match}}}`;
+		},
+	);
 
-  return (
-    <p
-      style={{
-        display: "flex",
-        position: "absolute",
-        transform: `translate(${props.x}px, ${props.y}px) rotate(${props.rotate}deg)`,
-        fontFamily: props.fontFamily,
-        fontWeight: props.fontWeight,
-        fontSize: `${props.fontSize}px`,
-        textTransform: props.textTransform,
-        width: `${props.width}px`,
-        height: `${props.height}px`,
-        color: addAlphaToHex(props.color, props.textColorOpacity),
-        overflowWrap: "break-word",
-        whiteSpace: "pre-wrap",
-        wordBreak: "break-word",
-        textAlign: props.textAlign,
-        justifyContent:
-          props.textAlign === "center"
-            ? "center"
-            : props.textAlign === "right"
-            ? "flex-end"
-            : "flex-start",
-        lineHeight: props.lineHeight,
-        filter: props.blur ? `blur(${props.blur}px)` : "none",
-      }}
-    >
-      {contentWithVariables}
-    </p>
-  );
+	return (
+		<p
+			style={{
+				display: "flex",
+				position: "absolute",
+				transform: `translate(${props.x}px, ${props.y}px) rotate(${props.rotate}deg)`,
+				fontFamily: props.fontFamily,
+				fontWeight: props.fontWeight,
+				fontSize: `${props.fontSize}px`,
+				textTransform: props.textTransform,
+				width: `${props.width}px`,
+				height: `${props.height}px`,
+				color: addAlphaToHex(props.color, props.textColorOpacity),
+				overflowWrap: "break-word",
+				whiteSpace: "pre-wrap",
+				wordBreak: "break-word",
+				textAlign: props.textAlign,
+				justifyContent:
+					props.textAlign === "center"
+						? "center"
+						: props.textAlign === "right"
+							? "flex-end"
+							: "flex-start",
+				lineHeight: props.lineHeight,
+				filter: props.blur ? `blur(${props.blur}px)` : "none",
+				textShadow:
+					props.textShadowXOffset &&
+					props.textShadowYOffset &&
+					props.textShadowBlur &&
+					props.textShadowColor
+						? `${props.textShadowXOffset}px ${props.textShadowYOffset}px ${props.textShadowBlur}px ${props.textShadowColor}`
+						: "none",
+			}}
+		>
+			{contentWithVariables}
+		</p>
+	);
 };
