2d3358be435876268a6ae89ab797dc5e72eb25e8
constantgillet <cpcdgillet@gmail.com>
2024-06-15 20:49:30 +0200

    Add text shadow component

--- app/components/PropertiesPanel.tsx
+++ app/components/PropertiesPanel.tsx
@@ -1,8 +1,8 @@
 import {
-  Text,
-  Grid,
-  Separator as RadixSeparator,
-  Tooltip,
+	Text,
+	Grid,
+	Separator as RadixSeparator,
+	Tooltip,
 } from "@radix-ui/themes";
 import { css, cx } from "styled-system/css";
 import { useEditorStore } from "../stores/EditorStore";
@@ -17,25 +17,26 @@ import { type Element, defaultElements } from "~/stores/elementTypes";
 import { BorderProperties } from "./BorderProperties";
 import { Icon } from "./Icon";
 import { BlurProperties } from "./BlurProperties";
+import { TextShadowProperties } from "./TextShadowProperties";
 
 const Separator = () => {
-  return (
-    <RadixSeparator
-      className={css({ w: "auto!important", marginY: "20px", mx: "-16px" })}
-    />
-  );
+	return (
+		<RadixSeparator
+			className={css({ w: "auto!important", marginY: "20px", mx: "-16px" })}
+		/>
+	);
 };
 
 type GroupedPropertiesType = {
-  [key: string]: Array<ValueType>;
+	[key: string]: Array<ValueType>;
 };
 
 export type ValueType = {
-  nodeId: string;
-  propertyName: string;
-  value: any;
-  variable?: boolean;
-  variableName?: string;
+	nodeId: string;
+	propertyName: string;
+	value: any;
+	variable?: boolean;
+	variableName?: string;
 };
 
 /**
@@ -44,292 +45,308 @@ export type ValueType = {
  * @returns GroupedPropertiesType
  */
 const GroupProperties = (nodes: Element[]) => {
-  const propertieFlatList: Array<ValueType> = [];
+	const propertieFlatList: Array<ValueType> = [];
 
-  for (const node of nodes) {
-    //Check if node has the default properties of the type
-    const defaultElement = defaultElements[node.type];
+	for (const node of nodes) {
+		//Check if node has the default properties of the type
+		const defaultElement = defaultElements[node.type];
 
-    //Add the missing properties to the node
-    if (defaultElement) {
-      for (const [key, value] of Object.entries(defaultElement)) {
-        if (node[key] === undefined) {
-          node[key] = value;
-        }
-      }
-    }
+		//Add the missing properties to the node
+		if (defaultElement) {
+			for (const [key, value] of Object.entries(defaultElement)) {
+				if (node[key] === undefined) {
+					node[key] = value;
+				}
+			}
+		}
 
-    //Use for of loop to iterate over the object
-    for (const [key, value] of Object.entries(node)) {
-      //ignore if value is undefined
-      if (value === undefined) {
-        return;
-      }
+		//Use for of loop to iterate over the object
+		for (const [key, value] of Object.entries(node)) {
+			//ignore if value is undefined
+			if (value === undefined) {
+				return;
+			}
 
-      //Check if the node has a variable
-      const variable = node.variables?.find(
-        (variable) => variable.property === key
-      );
+			//Check if the node has a variable
+			const variable = node.variables?.find(
+				(variable) => variable.property === key,
+			);
 
-      propertieFlatList.push({
-        nodeId: node.id,
-        propertyName: key,
-        value: value,
-        variable: variable ? true : false,
-        variableName: variable ? variable.name : undefined,
-      });
-    }
-  }
+			propertieFlatList.push({
+				nodeId: node.id,
+				propertyName: key,
+				value: value,
+				variable: variable ? true : false,
+				variableName: variable ? variable.name : undefined,
+			});
+		}
+	}
 
-  //group by property by name
-  const groupedProperties = propertieFlatList.reduce(
-    (acc: GroupedPropertiesType, property) => {
-      const propertyName = property.propertyName;
+	//group by property by name
+	const groupedProperties = propertieFlatList.reduce(
+		(acc: GroupedPropertiesType, property) => {
+			const propertyName = property.propertyName;
 
-      if (!acc[propertyName]) {
-        acc[propertyName] = [];
-      }
+			if (!acc[propertyName]) {
+				acc[propertyName] = [];
+			}
 
-      acc[propertyName].push(property);
+			acc[propertyName].push(property);
 
-      return acc;
-    },
-    {}
-  );
+			return acc;
+		},
+		{},
+	);
 
-  return groupedProperties;
+	return groupedProperties;
 };
 
 export const PropertiesPanel = () => {
-  const selected = useEditorStore((state) => state.selected);
-  const tree = useEditorStore((state) => state.tree);
+	const selected = useEditorStore((state) => state.selected);
+	const tree = useEditorStore((state) => state.tree);
 
-  const selectedNodes = tree.children.filter((node) =>
-    selected.includes(node.id)
-  );
+	const selectedNodes = tree.children.filter((node) =>
+		selected.includes(node.id),
+	);
 
-  //Get the properties list of the selected node
-  const properties = GroupProperties(selectedNodes);
+	//Get the properties list of the selected node
+	const properties = GroupProperties(selectedNodes);
 
-  console.log(properties);
+	console.log(properties);
 
-  return (
-    <aside
-      className={cx(
-        css({
-          w: 268,
-          backgroundColor: "var(--color-panel)",
-          borderColor: "var(--global-color-border, currentColor)",
-          padding: "var(--space-4)",
-          borderLeft: "1px solid var(--gray-a5)",
-          height: "calc(100vh - 58px)",
-          overflowY: "auto",
-        }),
-        "hidde-scrollbar"
-      )}
-    >
-      {properties?.x &&
-        properties?.y &&
-        properties?.width &&
-        properties?.height &&
-        properties?.rotate && (
-          <>
-            <PositionAndSizeProperties
-              properties={{
-                x: properties.x,
-                y: properties.y,
-                width: properties.width,
-                height: properties.height,
-                rotate: properties.rotate,
-              }}
-            />
-            <Separator />
-          </>
-        )}
-      {properties?.backgroundColor && properties?.backgroundOpacity && (
-        <>
-          <BackgroundColorProperties
-            properties={{
-              backgroundColor: properties.backgroundColor,
-              backgroundOpacity: properties.backgroundOpacity,
-            }}
-          />
-          <Separator />
-        </>
-      )}
-      {properties?.src && properties?.objectFit && (
-        <>
-          <ImageItemsProperties
-            properties={{
-              src: properties.src,
-              objectFit: properties.objectFit,
-            }}
-          />
-          <Separator />
-        </>
-      )}
-      {properties?.color && properties?.textColorOpacity && (
-        <>
-          <TextColorProperties
-            properties={{
-              color: properties.color,
-              textColorOpacity: properties.textColorOpacity,
-            }}
-          />
-          <Separator />
-        </>
-      )}
-      {properties?.fontSize &&
-        properties?.textAlign &&
-        properties?.fontWeight &&
-        properties?.fontStyle &&
-        properties?.textTransform &&
-        properties?.lineHeight && (
-          <>
-            <TextProperties
-              properties={{
-                fontFamily: properties.fontFamily,
-                fontSize: properties.fontSize,
-                textAlign: properties.textAlign,
-                fontWeight: properties.fontWeight,
-                fontStyle: properties.fontStyle,
-                textTransform: properties.textTransform,
-                lineHeight: properties.lineHeight,
-              }}
-            />
-            <Separator />
-          </>
-        )}
+	return (
+		<aside
+			className={cx(
+				css({
+					w: 268,
+					backgroundColor: "var(--color-panel)",
+					borderColor: "var(--global-color-border, currentColor)",
+					padding: "var(--space-4)",
+					borderLeft: "1px solid var(--gray-a5)",
+					height: "calc(100vh - 58px)",
+					overflowY: "auto",
+				}),
+				"hidde-scrollbar",
+			)}
+		>
+			{properties?.x &&
+				properties?.y &&
+				properties?.width &&
+				properties?.height &&
+				properties?.rotate && (
+					<>
+						<PositionAndSizeProperties
+							properties={{
+								x: properties.x,
+								y: properties.y,
+								width: properties.width,
+								height: properties.height,
+								rotate: properties.rotate,
+							}}
+						/>
+						<Separator />
+					</>
+				)}
+			{properties?.backgroundColor && properties?.backgroundOpacity && (
+				<>
+					<BackgroundColorProperties
+						properties={{
+							backgroundColor: properties.backgroundColor,
+							backgroundOpacity: properties.backgroundOpacity,
+						}}
+					/>
+					<Separator />
+				</>
+			)}
+			{properties?.src && properties?.objectFit && (
+				<>
+					<ImageItemsProperties
+						properties={{
+							src: properties.src,
+							objectFit: properties.objectFit,
+						}}
+					/>
+					<Separator />
+				</>
+			)}
+			{properties?.color && properties?.textColorOpacity && (
+				<>
+					<TextColorProperties
+						properties={{
+							color: properties.color,
+							textColorOpacity: properties.textColorOpacity,
+						}}
+					/>
+					<Separator />
+				</>
+			)}
+			{properties?.fontSize &&
+				properties?.textAlign &&
+				properties?.fontWeight &&
+				properties?.fontStyle &&
+				properties?.textTransform &&
+				properties?.lineHeight && (
+					<>
+						<TextProperties
+							properties={{
+								fontFamily: properties.fontFamily,
+								fontSize: properties.fontSize,
+								textAlign: properties.textAlign,
+								fontWeight: properties.fontWeight,
+								fontStyle: properties.fontStyle,
+								textTransform: properties.textTransform,
+								lineHeight: properties.lineHeight,
+							}}
+						/>
+						<Separator />
+					</>
+				)}
 
-      {properties?.borderTopLeftRadius &&
-        properties?.borderTopRightRadius &&
-        properties?.borderBottomLeftRadius &&
-        properties?.borderBottomRightRadius && (
-          <>
-            <RadiusProperties
-              properties={{
-                borderTopLeftRadius: properties.borderTopLeftRadius,
-                borderTopRightRadius: properties.borderTopRightRadius,
-                borderBottomLeftRadius: properties.borderBottomLeftRadius,
-                borderBottomRightRadius: properties.borderBottomRightRadius,
-              }}
-            />
-            <Separator />
-          </>
-        )}
-      {properties?.borderColor !== undefined &&
-        properties?.borderWidth !== undefined &&
-        properties?.borderType !== undefined && (
-          <>
-            <BorderProperties
-              properties={{
-                borderColor: properties.borderColor,
-                borderWidth: properties.borderWidth,
-                borderType: properties.borderType,
-              }}
-            />
-            <Separator />
-          </>
-        )}
-      {properties?.blur !== undefined && (
-        <>
-          <BlurProperties
-            properties={{
-              blur: properties.blur,
-            }}
-          />
-          <Separator />
-        </>
-      )}
-      {
-        // If empty properties
-        !Object.keys(properties).length && (
-          <>
-            <BackgroundColorProperties
-              properties={{
-                backgroundColor: [
-                  {
-                    value: tree.backgroundColor,
-                    nodeId: "1",
-                    propertyName: "backgroundColor",
-                  },
-                ],
-                backgroundOpacity: [
-                  {
-                    value: 1,
-                    nodeId: "1",
-                    propertyName: "backgroundOpacity",
-                  },
-                ],
-              }}
-            />
-            <Separator />
-            <VariablesFoundIndicator />
-            <Separator />
-          </>
-        )
-      }
-    </aside>
-  );
+			{properties?.borderTopLeftRadius &&
+				properties?.borderTopRightRadius &&
+				properties?.borderBottomLeftRadius &&
+				properties?.borderBottomRightRadius && (
+					<>
+						<RadiusProperties
+							properties={{
+								borderTopLeftRadius: properties.borderTopLeftRadius,
+								borderTopRightRadius: properties.borderTopRightRadius,
+								borderBottomLeftRadius: properties.borderBottomLeftRadius,
+								borderBottomRightRadius: properties.borderBottomRightRadius,
+							}}
+						/>
+						<Separator />
+					</>
+				)}
+			{properties?.borderColor !== undefined &&
+				properties?.borderWidth !== undefined &&
+				properties?.borderType !== undefined && (
+					<>
+						<BorderProperties
+							properties={{
+								borderColor: properties.borderColor,
+								borderWidth: properties.borderWidth,
+								borderType: properties.borderType,
+							}}
+						/>
+						<Separator />
+					</>
+				)}
+			{properties?.blur !== undefined && (
+				<>
+					<BlurProperties
+						properties={{
+							blur: properties.blur,
+						}}
+					/>
+					<Separator />
+				</>
+			)}
+			{properties?.textShadowXOffset !== undefined &&
+				properties?.textShadowYOffset !== undefined &&
+				properties?.textShadowColor !== undefined &&
+				properties?.textShadowBlur !== undefined && (
+					<>
+						<TextShadowProperties
+							properties={{
+								textShadowXOffset: properties.textShadowXOffset,
+								textShadowYOffset: properties.textShadowYOffset,
+								textShadowColor: properties.textShadowColor,
+								textShadowBlur: properties.textShadowBlur,
+							}}
+						/>
+						<Separator />
+					</>
+				)}
+			{
+				// If empty properties
+				!Object.keys(properties).length && (
+					<>
+						<BackgroundColorProperties
+							properties={{
+								backgroundColor: [
+									{
+										value: tree.backgroundColor,
+										nodeId: "1",
+										propertyName: "backgroundColor",
+									},
+								],
+								backgroundOpacity: [
+									{
+										value: 1,
+										nodeId: "1",
+										propertyName: "backgroundOpacity",
+									},
+								],
+							}}
+						/>
+						<Separator />
+						<VariablesFoundIndicator />
+						<Separator />
+					</>
+				)
+			}
+		</aside>
+	);
 };
 
 type PanelGroupProps = {
-  title: string;
-  children: React.ReactNode;
-  isOptional?: boolean;
-  handleClickAdd?: () => void;
-  handleClickRemove?: () => void;
-  hasValues?: boolean;
+	title: string;
+	children: React.ReactNode;
+	isOptional?: boolean;
+	handleClickAdd?: () => void;
+	handleClickRemove?: () => void;
+	hasValues?: boolean;
 };
 
 export const PanelGroup = (props: PanelGroupProps) => {
-  return (
-    <div
-      className={css({
-        display: "flex",
-        flexDirection: "column",
-        spaceY: "var(--space-3)",
-        fontWeight: "medium",
-      })}
-    >
-      <div
-        className={css({
-          display: "flex",
-          justifyContent: "space-between",
-        })}
-      >
-        <Text
-          size="2"
-          className={css({
-            display: "flex",
-          })}
-        >
-          {props.title}
-        </Text>
-        {props.isOptional &&
-          (props.hasValues ? (
-            <Tooltip content="Delete property">
-              <Icon
-                name="remove"
-                className={css({
-                  cursor: "pointer",
-                })}
-                onClick={props.handleClickRemove}
-              />
-            </Tooltip>
-          ) : (
-            <Tooltip content="Add property">
-              <Icon
-                name="add"
-                className={css({
-                  cursor: "pointer",
-                })}
-                onClick={props.handleClickAdd}
-              />
-            </Tooltip>
-          ))}
-      </div>
-      {props.children}
-    </div>
-  );
+	return (
+		<div
+			className={css({
+				display: "flex",
+				flexDirection: "column",
+				spaceY: "var(--space-3)",
+				fontWeight: "medium",
+			})}
+		>
+			<div
+				className={css({
+					display: "flex",
+					justifyContent: "space-between",
+				})}
+			>
+				<Text
+					size="2"
+					className={css({
+						display: "flex",
+					})}
+				>
+					{props.title}
+				</Text>
+				{props.isOptional &&
+					(props.hasValues ? (
+						<Tooltip content="Delete property">
+							<Icon
+								name="remove"
+								className={css({
+									cursor: "pointer",
+								})}
+								onClick={props.handleClickRemove}
+							/>
+						</Tooltip>
+					) : (
+						<Tooltip content="Add property">
+							<Icon
+								name="add"
+								className={css({
+									cursor: "pointer",
+								})}
+								onClick={props.handleClickAdd}
+							/>
+						</Tooltip>
+					))}
+			</div>
+			{props.children}
+		</div>
+	);
 };
---
+++ app/components/TextShadowProperties.tsx
@@ -0,0 +1,364 @@
+import { Box, Flex, Grid, Popover, TextField } from "@radix-ui/themes";
+import { PanelGroup, type ValueType } from "./PropertiesPanel";
+import { Icon } from "./Icon";
+import { type ChangeEvent, useEffect, useMemo, useState } from "react";
+import { arePropertiesTheSame } from "~/utils/arePropertiesTheSame";
+import { useEditorStore } from "../stores/EditorStore";
+import { getVarFromString } from "~/utils/getVarFromString";
+import { getElementVariables } from "~/stores/actions/getElementVariables";
+import { PropertyTextField } from "./PropertyTextField";
+import { groupBySameColor } from "~/utils/groupBySameColor";
+import { css } from "styled-system/css";
+import * as PopoverRadix from "@radix-ui/react-popover";
+import { updateElementsVariables } from "~/stores/actions/updateElementsVariables";
+import { getVariablesWithoutProperty } from "~/utils/getVariablesWithoutProperty";
+import * as SelectPicker from "react-color";
+import { grid, gridItem } from "styled-system/patterns";
+
+type TextShadowPropertiesProps = {
+	properties: {
+		textShadowColor: ValueType[];
+		textShadowXOffset: ValueType[];
+		textShadowYOffset: ValueType[];
+		textShadowBlur: ValueType[];
+	};
+};
+
+//Check if properties values are different from null
+const propertiesHaveValues = (properties: ValueType[]) => {
+	return properties.some((property) => property.value !== null);
+};
+export const TextShadowProperties = (props: TextShadowPropertiesProps) => {
+	const updateElements = useEditorStore((state) => state.updateElements);
+	const hasValues =
+		propertiesHaveValues(props.properties.textShadowXOffset) &&
+		propertiesHaveValues(props.properties.textShadowYOffset) &&
+		propertiesHaveValues(props.properties.textShadowColor) &&
+		propertiesHaveValues(props.properties.textShadowBlur);
+
+	const [colorValues, setColorValues] = useState(
+		groupBySameColor(
+			props.properties.textShadowColor,
+			props.properties.textShadowColor.map((property) => property.value),
+		),
+	);
+
+	useEffect(() => {
+		setColorValues(
+			groupBySameColor(
+				props.properties.textShadowColor,
+				props.properties.textShadowColor.map((property) => property.value),
+			),
+		);
+	}, [props.properties.textShadowColor]);
+
+	const setDefaultValueFromProps = (
+		property: keyof TextShadowPropertiesProps["properties"],
+	) => {
+		return arePropertiesTheSame(props.properties[property]) &&
+			!props.properties[property][0].variable
+			? props.properties[property][0].value
+			: arePropertiesTheSame(props.properties[property])
+				? `{{${props.properties[property][0].variableName}}}`
+				: "Mixed";
+	};
+
+	const [textShadowXOffsetValue, settextShadowXOffset] = useState(
+		setDefaultValueFromProps("textShadowXOffset"),
+	);
+
+	useEffect(() => {
+		settextShadowXOffset(setDefaultValueFromProps("textShadowXOffset"));
+	}, [props.properties.textShadowXOffset]);
+
+	const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter") {
+			applyPropertytextShadowXOffset(
+				e as unknown as ChangeEvent<HTMLInputElement>,
+			);
+		}
+	};
+
+	const applyPropertytextShadowXOffset = (
+		event: React.ChangeEvent<HTMLInputElement>,
+	) => {
+		const newValue = event.target.value;
+
+		const variableName = getVarFromString(newValue);
+
+		if (variableName && variableName.length > 0) {
+			updateElements(
+				props.properties.textShadowXOffset.map((property) => {
+					const currentVariables = getElementVariables(property.nodeId);
+
+					//Create new vaiables with the new variable if it doesn't exist
+					const newVariablesWithoutProperty = currentVariables.filter(
+						(variable) => variable.property !== property.propertyName,
+					);
+
+					const newVariables = [
+						...newVariablesWithoutProperty,
+						{
+							property: property.propertyName,
+							name: variableName,
+						},
+					];
+
+					return {
+						id: property.nodeId,
+						variables: newVariables,
+					};
+				}),
+				true,
+			);
+
+			return;
+		}
+
+		if (Number.isNaN(Number(newValue))) {
+			return;
+		}
+
+		const value = Number(newValue);
+
+		updateElements(
+			props.properties.textShadowXOffset.map((property) => {
+				const newVariablesWithoutProperty = getVariablesWithoutProperty(
+					property.propertyName,
+					property.nodeId,
+				);
+
+				return {
+					id: property.nodeId,
+					[property.propertyName]: value,
+					variables: newVariablesWithoutProperty,
+				};
+			}),
+			true,
+		);
+
+		event.target.blur();
+	};
+
+	const addDefault = () => {
+		updateElements(
+			props.properties.textShadowXOffset.map((property) => {
+				return {
+					id: property.nodeId,
+					textShadowXOffset: 1,
+					textShadowYOffset: 1,
+					textShadowColor: "#000000",
+					textShadowBlur: 1,
+				};
+			}),
+			true,
+		);
+	};
+
+	const removeDefault = () => {
+		updateElements(
+			props.properties.textShadowXOffset.map((property) => {
+				return {
+					id: property.nodeId,
+					textShadowXOffset: null,
+					borderType: null,
+					textShadowColor: null,
+				};
+			}),
+			true,
+		);
+	};
+
+	return (
+		<PanelGroup
+			title="Text Shadow"
+			isOptional
+			handleClickAdd={addDefault}
+			handleClickRemove={removeDefault}
+			hasValues={hasValues}
+		>
+			{hasValues ? (
+				<>
+					<Grid columns="2" gap="2" width="auto">
+						<Box>
+							<PropertyTextField
+								icon={<Icon name="border-outside" />}
+								placeholder="Border width"
+								hasVariable={
+									props.properties.textShadowXOffset[0].variable || false
+								}
+								value={textShadowXOffsetValue}
+								onChange={(e) => settextShadowXOffset(e.target.value)}
+								onBlur={applyPropertytextShadowXOffset}
+								onKeyUp={onKeyUp}
+							/>
+						</Box>
+						<Box>
+							<PropertyTextField
+								icon={<Icon name="border-outside" />}
+								placeholder="Border width"
+								hasVariable={
+									props.properties.textShadowXOffset[0].variable || false
+								}
+								value={textShadowXOffsetValue}
+								onChange={(e) => settextShadowXOffset(e.target.value)}
+								onBlur={applyPropertytextShadowXOffset}
+								onKeyUp={onKeyUp}
+							/>
+						</Box>
+					</Grid>
+					{colorValues.map((color) => (
+						<ColorLine
+							key={color.elementIds[0]}
+							color={{
+								elementIds: color.elementIds,
+								value: color.value,
+								colorVariable: color.colorVariable,
+							}}
+						/>
+					))}
+				</>
+			) : null}
+		</PanelGroup>
+	);
+};
+
+type ColorLineProps = {
+	color: {
+		elementIds: string[];
+		value: string;
+		colorVariable?: string | undefined;
+	};
+};
+
+const ColorLine = ({ color }: ColorLineProps) => {
+	const updateElements = useEditorStore((state) => state.updateElements);
+
+	const [colorValue, setColorValue] = useState(
+		color?.colorVariable ? `{{${color.colorVariable}}}` : color.value,
+	);
+
+	useEffect(() => {
+		setColorValue(
+			color?.colorVariable ? `{{${color.colorVariable}}}` : color.value,
+		);
+	}, [color.value, color.colorVariable]);
+
+	const applyColor = (newColor: string, saveToHistory = false) => {
+		const elementIds = color.elementIds;
+
+		updateElements(
+			elementIds.map((elementId) => ({
+				id: elementId,
+				textShadowColor: newColor,
+			})),
+			saveToHistory,
+		);
+	};
+
+	const applyColorInput = (newColorValue: string) => {
+		const elementIds = color.elementIds;
+
+		const variableName = getVarFromString(newColorValue);
+
+		if (variableName && variableName.length > 0) {
+			updateElementsVariables(elementIds, "textShadowColor", variableName);
+			return;
+		}
+
+		//Check if the color is hex color and valid
+		if (!/^#[0-9A-F]{6}$/i.test(newColorValue)) {
+			return;
+		}
+
+		updateElements(
+			elementIds.map((elementId) => {
+				const newVariablesWithoutProperty = getVariablesWithoutProperty(
+					"textShadowColor",
+					elementId,
+				);
+
+				return {
+					id: elementId,
+					textShadowColor: newColorValue,
+					variables: newVariablesWithoutProperty,
+				};
+			}),
+			true,
+		);
+	};
+
+	return (
+		<Flex
+			className={css({
+				alignItems: "center",
+				gap: 2,
+			})}
+		>
+			<Popover.Root>
+				<>
+					<PopoverRadix.Anchor>
+						<div className={grid({ columns: 12, gap: 2 })}>
+							<div className={gridItem({ colSpan: 7 })}>
+								<PropertyTextField
+									icon={
+										<Popover.Trigger onClick={(e) => e.stopPropagation()}>
+											<button
+												className={css({
+													width: "24px",
+													height: "20px",
+													flexShrink: 0,
+													_hover: {
+														cursor: "pointer",
+													},
+													borderRadius: "3px",
+												})}
+												style={{
+													backgroundColor: color.value,
+												}}
+											/>
+										</Popover.Trigger>
+									}
+									hasVariable={!!color.colorVariable}
+									placeholder="color hex"
+									value={colorValue}
+									onChange={(e) => setColorValue(e.target.value)}
+									onBlur={(e) => {
+										applyColorInput(e.target.value);
+									}}
+									onKeyUp={(e) => {
+										if (e.key === "Enter") {
+											applyColorInput(e.currentTarget.value);
+										}
+									}}
+								/>
+							</div>
+						</div>
+					</PopoverRadix.Anchor>
+					<Popover.Content side="left">
+						<SelectPicker.SketchPicker
+							disableAlpha
+							styles={{
+								default: {
+									picker: {
+										boxShadow: "none",
+									},
+								},
+							}}
+							className={css({
+								background: "var(--colors-background)!important",
+							})}
+							color={color.value}
+							onChange={(newColor) => {
+								applyColor(newColor.hex);
+							}}
+							onChangeComplete={(newColor) => {
+								applyColor(newColor.hex, true);
+							}}
+						/>
+					</Popover.Content>
+				</>
+			</Popover.Root>
+		</Flex>
+	);
+};
