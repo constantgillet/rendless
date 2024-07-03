fb3f7f030d90c306caefe7e1e96cf3767f84422e
constantgillet <cpcdgillet@gmail.com>
2024-07-03 23:21:23 +0200

    add LEMONSQUEEZY_WEBHOOK_SECRET env

--- app/libs/environment.server.ts
+++ app/libs/environment.server.ts
@@ -1,19 +1,20 @@
 import * as z from "zod";
 
 const environmentSchema = z.object({
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
-  WEBSITE_URL: z.string().min(1),
-  DATABASE_URL: z.string().min(1),
-  REDIS_URL: z.string().optional(),
-  BUCKET_URL: z.string().min(1),
-  BUCKET_NAME: z.string().min(1),
-  IMAGES_FOLDER: z.string().optional().default("ogimages/cached/"),
-  SPACES_KEY: z.string().min(1),
-  SPACES_SECRET: z.string().min(1),
-  RESEND_API_KEY: z.string().min(1),
-  EMAIL_FROM: z.string().email().default("noreply@rendless.com")
+	NODE_ENV: z
+		.enum(["development", "production", "test"])
+		.default("development"),
+	WEBSITE_URL: z.string().min(1),
+	DATABASE_URL: z.string().min(1),
+	REDIS_URL: z.string().optional(),
+	BUCKET_URL: z.string().min(1),
+	BUCKET_NAME: z.string().min(1),
+	IMAGES_FOLDER: z.string().optional().default("ogimages/cached/"),
+	SPACES_KEY: z.string().min(1),
+	SPACES_SECRET: z.string().min(1),
+	RESEND_API_KEY: z.string().min(1),
+	EMAIL_FROM: z.string().email().default("noreply@rendless.com"),
+	LEMONSQUEEZY_WEBHOOK_SECRET: z.string().min(1),
 });
 
 const environment = () => environmentSchema.parse(process.env);
