c47bdf26dff7187cf0addc20b39228b4d8f2d5e4
constantgillet <cpcdgillet@gmail.com>
2024-07-07 21:15:00 +0200

    Add linear and radial in properties

--- app/components/BackgroundGradientProperties.tsx
+++ app/components/BackgroundGradientProperties.tsx
@@ -1,6 +1,10 @@
 import { propertiesHaveValues } from "~/utils/propertiesHaveValues";
 import { PanelGroup, type ValueType } from "./PropertiesPanel";
 import { useEditorStore } from "~/stores/EditorStore";
+import { Box, Button, Grid, Select } from "@radix-ui/themes";
+import { PropertyTextField } from "./PropertyTextField";
+import { Icon } from "./Icon";
+import * as SelectPrimitive from "@radix-ui/react-select";
 
 type BackgroundGradientPropertiesProps = {
 	properties: {
@@ -31,7 +35,42 @@ export const BackgroundGradientProperties = (
 			handleClickRemove={() => {}}
 			hasValues={hasValues}
 		>
-			{hasValues ? <>values</> : null}
+			{hasValues ? (
+				<>
+					<Grid columns="2" gap="2" width="auto">
+						<Box>
+							<Select.Root
+								onValueChange={(newValue) => {
+									console.log(newValue);
+								}}
+							>
+								<SelectPrimitive.Trigger>
+									<Button variant="surface" size={"2"} color="gray">
+										<Icon name="chevron-down" />
+									</Button>
+								</SelectPrimitive.Trigger>
+								<Select.Content position="popper" align="end">
+									<Select.Item value={"linear"}>Linear</Select.Item>
+									<Select.Item value={"radial"}>Radial</Select.Item>
+								</Select.Content>
+							</Select.Root>
+						</Box>
+						<Box>
+							<PropertyTextField
+								icon={<Icon name="rotate-left" />}
+								placeholder="Angle"
+								hasVariable={
+									props.properties.backgroundGradientAngle[0].variable || false
+								}
+								value={"0"}
+								onChange={(e) => {}}
+								onBlur={(e) => {}}
+								onKeyUp={(e) => {}}
+							/>
+						</Box>
+					</Grid>
+				</>
+			) : null}
 		</PanelGroup>
 	);
 };
