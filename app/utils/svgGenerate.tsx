4c7cf6965b064803ff58f15532e121f68716607a
constantgillet <cpcdgillet@gmail.com>
2024-07-21 14:01:07 +0200

    fix italic

--- app/utils/svgGenerate.tsx
+++ app/utils/svgGenerate.tsx
@@ -9,128 +9,130 @@ import { addVariablesValuesToElementProperties } from "./addVariablesValuesToEle
 import type { ElementWithVariables } from "~/stores/elementTypes";
 
 const fetch = NodeFetchCache.create({
-  cache: new FileSystemCache({
-    cacheDirectory: "./.cache/fetch",
-    // Time to live. How long (in ms) responses remain cached before being
-    // automatically ejected. If undefined, responses are never
-    // automatically ejected from the cache.
-    ttl: 1000 * 60 * 60 * 24 * 7, // 1 week
-  }),
+	cache: new FileSystemCache({
+		cacheDirectory: "./.cache/fetch",
+		// Time to live. How long (in ms) responses remain cached before being
+		// automatically ejected. If undefined, responses are never
+		// automatically ejected from the cache.
+		ttl: 1000 * 60 * 60 * 24 * 7, // 1 week
+	}),
 });
 
 async function fetchFont(
-  font: string,
-  weight: number,
-  fontStyle: "normal" | "italic" = "normal"
+	font: string,
+	weight: number,
+	fontStyle: "normal" | "italic" = "normal",
 ): Promise<ArrayBuffer | null> {
-  const API = `https://fonts.googleapis.com/css2?family=${font}:wght@${weight}`;
+	const API = `https://fonts.googleapis.com/css2?family=${font}:${
+		fontStyle === "italic" ? "ital," : ""
+	}wght@${fontStyle === "italic" ? "1," : ""}${weight}`; //,1 is italic weight
 
-  const css = await (
-    await fetch(API, {
-      headers: {
-        // Make sure it returns TTF.
-        "User-Agent":
-          "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
-      },
-    })
-  ).text();
+	const css = await (
+		await fetch(API, {
+			headers: {
+				// Make sure it returns TTF.
+				"User-Agent":
+					"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+			},
+		})
+	).text();
 
-  const resource = css.match(
-    /src: url\((.+)\) format\('(opentype|truetype)'\)/
-  );
+	const resource = css.match(
+		/src: url\((.+)\) format\('(opentype|truetype)'\)/,
+	);
 
-  if (!resource) return null;
+	if (!resource) return null;
 
-  const res = await fetch(resource[1]);
+	const res = await fetch(resource[1]);
 
-  return res.arrayBuffer();
+	return res.arrayBuffer();
 }
 
 const getAllFonts = async (tree: Tree) => {
-  const fontList = tree.children
-    .filter((child) => child.type === "text")
-    .map((child) => ({
-      fontFamily: child.fontFamily,
-      fontWeight: child.fontWeight,
-      fontStyle: child.fontStyle,
-    }));
+	const fontList = tree.children
+		.filter((child) => child.type === "text")
+		.map((child) => ({
+			fontFamily: child.fontFamily,
+			fontWeight: child.fontWeight,
+			fontStyle: child.fontStyle,
+		}));
 
-  const fonts: Font[] = [];
+	const fonts: Font[] = [];
 
-  for (const font of fontList) {
-    const buffer = await fetchFont(
-      font.fontFamily,
-      font.fontWeight,
-      font.fontStyle
-    );
-    if (!buffer) {
-      throw new Error("Font not found");
-    }
+	for (const font of fontList) {
+		const buffer = await fetchFont(
+			font.fontFamily,
+			font.fontWeight,
+			font.fontStyle,
+		);
+		if (!buffer) {
+			throw new Error("Font not found");
+		}
 
-    fonts.push({
-      name: font.fontFamily,
-      data: buffer,
-      weight: font.fontWeight,
-      style: font.fontStyle,
-    });
-  }
+		fonts.push({
+			name: font.fontFamily,
+			data: buffer,
+			weight: font.fontWeight,
+			style: font.fontStyle,
+		});
+	}
 
-  return fonts;
+	return fonts;
 };
 
 export type VariablesValues = { name: string; value: string }[];
 
 export const SvgGenerate = async (
-  tree: Tree,
-  variablesValues: VariablesValues
+	tree: Tree,
+	variablesValues: VariablesValues,
 ) => {
-  const fontsLoaded = await getAllFonts(tree);
+	const fontsLoaded = await getAllFonts(tree);
 
-  const svg = await satori(
-    <TreeToJsx tree={tree} variablesValues={variablesValues} />,
-    {
-      width: 1200,
-      height: 630,
-      fonts: fontsLoaded,
-    }
-  );
+	const svg = await satori(
+		<TreeToJsx tree={tree} variablesValues={variablesValues} />,
+		{
+			width: 1200,
+			height: 630,
+			fonts: fontsLoaded,
+		},
+	);
 
-  return svg;
+	return svg;
 };
 
 const renderedComponentsMap = {
-  rect: RectElementRendered,
-  text: TextElementRendered,
-  image: ImageElementRendered,
+	rect: RectElementRendered,
+	text: TextElementRendered,
+	image: ImageElementRendered,
 };
 
 const TreeToJsx = ({
-  tree,
-  variablesValues,
+	tree,
+	variablesValues,
 }: {
-  tree: Tree;
-  variablesValues: VariablesValues;
+	tree: Tree;
+	variablesValues: VariablesValues;
 }) => {
-  return (
-    <div
-      style={{
-        backgroundColor: tree.backgroundColor,
-        width: 1200,
-        height: 630,
-        position: "relative",
-        display: "flex",
-      }}
-    >
-      {tree.children.map((child) => {
-        const Component = renderedComponentsMap[child.type];
+	return (
+		<div
+			style={{
+				backgroundColor: tree.backgroundColor,
+				width: 1200,
+				height: 630,
+				position: "relative",
+				display: "flex",
+			}}
+		>
+			{tree.children.map((child) => {
+				const Component = renderedComponentsMap[child.type];
 
-        const propsWithVariables = addVariablesValuesToElementProperties(
-          child as ElementWithVariables,
-          variablesValues
-        );
+				const propsWithVariables = addVariablesValuesToElementProperties(
+					child as ElementWithVariables,
+					variablesValues,
+				);
 
-        return <Component key={child.id} {...propsWithVariables} />;
-      })}
-    </div>
-  );
+				return <Component key={child.id} {...propsWithVariables} />;
+			})}
+		</div>
+	);
 };
